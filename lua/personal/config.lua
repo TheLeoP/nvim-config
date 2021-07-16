c32b4316bdcc3f1ed19502db5470d291dc93a730
TheLeoP <eugenio2305@hotmail.com>
2021-07-16 16:36:02 -0500

    Descomentado hover() y comentado signature_help()

--- lua/personal/config.lua
+++ lua/personal/config.lua
@@ -63,11 +63,11 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
   }
 )
 
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
---   vim.lsp.handlers.hover, {
---     border = { '┏', '━', '┓', '┃', '┛', '━', '┗', '┃' },
---   }
--- )
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = { '┏', '━', '┓', '┃', '┛', '━', '┗', '┃' },
+  }
+)
 
 
 -- configuración LSP
