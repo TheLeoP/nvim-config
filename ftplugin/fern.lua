b18a0a7cd83cc17b6b54b45b89237db768b15e19
TheLeoP <eugenio2305@hotmail.com>
2022-11-08 13:01:06 -0500

    update

--- ftplugin/fern.lua
+++ ftplugin/fern.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<space>", "<Plug>(fern-action-mark)", { buffer = true })
+vim.keymap.set("n", "<c-space>", "<Plug>(fern-action-mark)", { buffer = true })
 vim.keymap.set("n", "-", "<Plug>(fern-action-leave)", { buffer = true })
 
 vim.fn["glyph_palette#apply"]()
