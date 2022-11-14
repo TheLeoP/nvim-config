6c050ae01cb3ea604549e6d3631aeb3461f318f7
TheLeoP <eugenio2305@hotmail.com>
2022-11-14 09:58:22 -0500

    update

--- lua/personal/config/signature.lua
+++ lua/personal/config/signature.lua
@@ -3,6 +3,7 @@ require("lsp_signature").setup {
   doc_lines = 0,
   floating_windows = true,
   floating_window_above_cur_line = true,
+  floating_window_off_x = 66,
   fix_pos = true,
   hint_enable = false,
   handler_opts = {
