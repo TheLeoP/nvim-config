dcba3a11a0f7d5634cbefc5f34d3ccaec5095ae5
TheLeoP <eugenio2305@hotmail.com>
2023-01-22 16:54:26 -0500

    update

--- after/plugin/highlight.lua
+++ after/plugin/highlight.lua
@@ -3,3 +3,7 @@ vim.api.nvim_set_hl(0, "DiagnosticSignHint", { link = "GruvboxFg2" })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { link = "GruvboxFg2" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = "#d5c4a1" })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "GruvboxFg2" })
+
+local highlight = vim.api.nvim_get_hl_by_name("Operator", true)
+highlight.italic = false
+vim.api.nvim_set_hl(0, "Operator", highlight)
