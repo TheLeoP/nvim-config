402e0f4cead0ffb2fa44608a6fce88c9e4713dbf
TheLeoP <eugenio2305@hotmail.com>
2023-01-23 22:48:27 -0500

    update

--- lua/personal/config/treesitter.lua
+++ lua/personal/config/treesitter.lua
@@ -5,7 +5,7 @@ end
 require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
   ignore_install = {
-    -- 'markdown',
+    "thrift",
   },
   highlight = {
     enable = true, -- false will disable the whole extension
