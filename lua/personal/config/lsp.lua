91d6529eeaff1b30e33205c8c774f77395cf81c8
luis <eugenio2305@hotmail.com>
2023-01-30 15:27:03 -0500

    update

--- lua/personal/config/lsp.lua
+++ lua/personal/config/lsp.lua
@@ -148,8 +148,8 @@ local null_ls = require "null-ls"
 null_ls.setup {
   on_attach = on_attach_general,
   sources = {
-    -- null_ls.builtins.diagnostics.eslint_d,
-    -- null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
@@ -237,6 +237,12 @@ lspconfig.jsonls.setup {
       },
     },
   },
+}
+
+-- angular
+lspconfig.angularls.setup {
+  on_attach = on_attach_general,
+  capabilities = capabilities,
 }
 
 -- java
