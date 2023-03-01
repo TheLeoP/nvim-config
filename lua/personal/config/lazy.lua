fb52a1c968c8ca2d6930944247d10292dc6e6b57
TheLeoP <eugenio2305@hotmail.com>
2023-03-01 12:45:22 -0500

    update

--- lua/personal/config/lazy.lua
+++ lua/personal/config/lazy.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -310,7 +311,7 @@ require("lazy").setup({
     end,
   },
   {
-    "feline-nvim/feline.nvim",
+    "freddiehaddad/feline.nvim",
     config = function()
       require "personal.config.feline"
     end,
@@ -393,6 +394,7 @@ require("lazy").setup({
             workspaces = {
               notes = "~/notes",
             },
+            default_workspace = "notes",
           },
         },
       },
