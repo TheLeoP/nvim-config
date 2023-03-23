4517a1331cae41e4f716b5a81402fc2f099ddb5b
TheLeoP <eugenio2305@hotmail.com>
2023-03-23 15:36:54 -0500

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -84,6 +84,12 @@ return {
   {
     "stevearc/dressing.nvim",
     opts = {
+      input = {
+        insert_only = false,
+        win_options = {
+          winblend = 0,
+        },
+      },
       select = {
         backend = { "telescope", "builtin" },
       },
