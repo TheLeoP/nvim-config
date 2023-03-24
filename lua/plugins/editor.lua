a5aaa08d5d7af48e732325a1c4d44fc7c2ec3412
TheLeoP <eugenio2305@hotmail.com>
2023-03-24 16:29:04 -0500

    updat

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -101,6 +101,10 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension "fzf"
+    end,
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
