cf932296d60b6e5119f21174c3257e4226e6741b
TheLeoP <eugenio2305@hotmail.com>
2024-02-29 08:22:22 -0500

    No auto preview on qf window

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -1,9 +1,13 @@
 return {
   {
     "kevinhwang91/nvim-bqf",
-    ft = "qf",
     opts = {
       func_map = { split = "<C-s>" },
+      preview = {
+        auto_preview = {
+          default = false,
+        },
+      },
     },
   },
 }
