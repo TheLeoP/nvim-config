81059f2927728914b785a1b76c4ce9f4094a6f4e
TheLeoP <eugenio2305@hotmail.com>
2024-02-29 08:32:29 -0500

    Automatically continue lists on norg files

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -1,1 +1,9 @@
 require("personal.util.writing").setup()
+
+-- automatically continue lists
+vim.opt_local.comments = {
+  "b:-",
+  "b:~",
+}
+vim.opt_local.formatoptions:append "r"
+vim.opt_local.formatoptions:remove "c"
