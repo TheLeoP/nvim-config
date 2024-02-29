f17e2c789fab9813e7e218032d00868ffd64d767
TheLeoP <eugenio2305@hotmail.com>
2024-02-29 18:56:11 -0500

    Continue more list on norg files

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -3,7 +3,13 @@ require("personal.util.writing").setup()
 -- automatically continue lists
 vim.opt_local.comments = {
   "b:-",
+  "b:--",
+  "b:---",
+  "b:----",
   "b:~",
+  "b:~~",
+  "b:~~~",
+  "b:~~~~",
 }
 vim.opt_local.formatoptions:append "r"
 vim.opt_local.formatoptions:remove "c"
