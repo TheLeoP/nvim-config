8883aa12f1d39135a7f539b45319d36a811c6f10
TheLeoP <eugenio2305@hotmail.com>
2024-03-22 09:56:51 -0500

    Update DAP C# configuration

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -474,6 +474,7 @@ return {
           name = "launch - netcoredbg",
           request = "launch",
           program = function() return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file") end,
+          cwd = "${workspaceFolder}",
         },
       }
     end,
