24b9e8ce1d42f33c07851e58448796bcc1df581a
TheLeoP <eugenio2305@hotmail.com>
2024-03-31 20:27:14 -0500

    Avoid error when calling custom ctrl-l on cmdline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -131,10 +131,10 @@ return {
         mode = "n",
         "<c-l>",
         function()
-          vim.cmd.nohlsearch()
-          vim.cmd.diffupdate()
-          require("notify").dismiss { silent = true, pending = true }
-          vim.cmd.normal { "\12", bang = true } -- ctrl-l
+          pcall(vim.cmd.nohlsearch)
+          pcall(vim.cmd.diffupdate)
+          pcall(require("notify").dismiss, { silent = true, pending = true })
+          pcall(vim.cmd.normal, { "\12", bang = true }) -- ctrl-l
         end,
       },
     },
