02fb564a387b634c852f751659adbe69b7f1a688
TheLeoP <eugenio2305@hotmail.com>
2024-04-25 20:27:54 -0500

    Remove extra table on pqf.lua

--- lua/plugins/pqf.lua
+++ lua/plugins/pqf.lua
@@ -1,10 +1,8 @@
 return {
-  {
-    "yorickpeterse/nvim-pqf",
-    event = "VeryLazy",
-    opts = {
-      show_multiple_lines = true,
-      max_filename_length = 40,
-    },
+  "yorickpeterse/nvim-pqf",
+  event = "VeryLazy",
+  opts = {
+    show_multiple_lines = true,
+    max_filename_length = 40,
   },
 }
