33ee7852c6f05949d65ec56a0acc5189b8cbb1af
TheLeoP <eugenio2305@hotmail.com>
2024-05-09 22:37:44 -0500

    Replace vim-dispatch with overseer

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,16 +1,55 @@
 local function open_and_close()
   local overseer = require "overseer"
 
-  -- Open the task window.
   overseer.open { enter = false }
 
-  -- Close it after 10 seconds (if not inside the window).
   vim.defer_fn(function()
     if vim.bo.filetype ~= "OverseerList" and vim.bo.buftype ~= "terminal" then overseer.close() end
   end, 10 * 1000)
 end
 
--- Task runner.
+local function restart_last_task()
+  local overseer = require "overseer"
+  local tasks = overseer.list_tasks { recent_first = true }
+  if vim.tbl_isempty(tasks) then
+    vim.notify("No tasks found", vim.log.levels.WARN)
+  else
+    overseer.run_action(tasks[1], "restart")
+    open_and_close()
+  end
+end
+
+local function run_task()
+  local overseer = require "overseer"
+  overseer.run_template({}, function(task)
+    if not task then return end
+    open_and_close()
+  end)
+end
+
+local function start_prompt()
+  local overseer = require "overseer"
+  vim.ui.input({ prompt = "cmd: " }, function(input)
+    if not input or input == "" then return end
+    overseer.run_template({
+      name = "shell",
+      prompt = "never",
+      params = {
+        cmd = input,
+        components = {
+          { "display_duration", detail_level = 2 },
+          { "on_exit_set_status", success_codes = { 0, 1 } },
+          "on_complete_notify",
+          "on_complete_dispose",
+        },
+      },
+    }, function(task)
+      if not task then return end
+      open_and_close()
+    end)
+  end)
+end
+
 return {
   {
     "stevearc/overseer.nvim",
@@ -45,24 +84,15 @@ return {
       },
     },
     config = function(_, opts)
-      require("overseer").setup(opts)
+      local overseer = require "overseer"
+      overseer.setup(opts)
 
       vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<cr>", { desc = "Toggle task window" })
-      vim.keymap.set("n", "<leader>o<", function()
-        local overseer = require "overseer"
-        local tasks = overseer.list_tasks { recent_first = true }
-        if vim.tbl_isempty(tasks) then
-          vim.notify("No tasks found", vim.log.levels.WARN)
-        else
-          overseer.run_action(tasks[1], "restart")
-          open_and_close()
-        end
-      end, { desc = "Restart last task" })
-      vim.keymap.set("n", "<leader>or", function()
-        require("overseer").run_template({}, function(task)
-          if task then open_and_close() end
-        end)
-      end, { desc = "Run task" })
+      vim.keymap.set("n", "<leader>o<", restart_last_task, { desc = "Restart last task" })
+      vim.keymap.set("n", "<leader>or", run_task, { desc = "Run task" })
+
+      -- vim-dispatch style keymaps
+      vim.keymap.set("n", "' ", start_prompt)
     end,
   },
 }
