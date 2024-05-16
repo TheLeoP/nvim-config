b57d9c3b770616945b6ebcface813ceac813b363
TheLeoP <eugenio2305@hotmail.com>
2024-05-16 14:32:49 -0500

    Add type to powershell.lua config

--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,6 +1,7 @@
 return {
   "TheLeoP/powershell.nvim",
   dev = true,
+  ---@type powershell.user_config
   opts = {
     capabilities = require("personal.config.lsp").capabilities,
     bundle_path = vim.fs.normalize(require("personal.config.lsp").mason_root .. "powershell-editor-services"),
