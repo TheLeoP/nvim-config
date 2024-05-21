2409fcf80c5a8c76532c4a71751faecf98298d0d
TheLeoP <eugenio2305@hotmail.com>
2024-05-20 22:31:00 -0500

    Add command DapVirtualTextClear

--- lua/plugins/nvim-dap-virtual-text.lua
+++ lua/plugins/nvim-dap-virtual-text.lua
@@ -1,4 +1,15 @@
 return {
   "theHamsta/nvim-dap-virtual-text",
   opts = { virt_text_pos = "eol" },
+  config = function(_, opts)
+    require("nvim-dap-virtual-text").setup(opts)
+    vim.api.nvim_create_user_command(
+      "DapVirtualTextClear",
+      function() require("nvim-dap-virtual-text.virtual_text").clear_virtual_text() end,
+      {
+        desc = "Clear all the virtual text displayed by nvim-dap-virtual-text",
+        force = true,
+      }
+    )
+  end,
 }
