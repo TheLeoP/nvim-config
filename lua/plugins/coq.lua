6bb973dc923b1ed39ce079ad0fae187e911ec96e
TheLeoP <eugenio2305@hotmail.com>
2024-05-22 23:13:06 -0500

    Revert completion mappings

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -100,25 +100,29 @@ return {
 
     vim.keymap.set("i", "<CR>", function()
       if vim.fn.pumvisible() == 1 then
-        if vim.fn.complete_info().selected == -1 then return "<C-e><CR>" end
+        if vim.fn.complete_info().selected == -1 then
+          return "<C-e><CR>"
+        else
+          return "<C-y>"
+        end
       else
         return "<CR>"
       end
     end, { expr = true, silent = true })
 
-    vim.keymap.set("i", "<c-n>", function()
+    vim.keymap.set("i", "<Tab>", function()
       if vim.fn.pumvisible() == 1 then
         return "<down>"
       else
-        return "<c-n>"
+        return "<Tab>"
       end
     end, { expr = true, silent = true })
 
-    vim.keymap.set("i", "<c-p>", function()
+    vim.keymap.set("i", "<s-tab>", function()
       if vim.fn.pumvisible() == 1 then
         return "<up>"
       else
-        return "<BS>"
+        return "<s-tab>"
       end
     end, { expr = true, silent = true })
 
