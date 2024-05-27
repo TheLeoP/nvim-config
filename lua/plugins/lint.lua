d5183c82fa49bb06da875ac76cc389c8297c6fcc
TheLeoP <eugenio2305@hotmail.com>
2024-05-27 18:05:42 -0500

    Clear nvim-lint autocmd on reload

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -15,13 +15,14 @@ return {
     }
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
       callback = function()
         if not lint_options.enabled then return end
         require("lint").try_lint(nil, { ignore_errors = true })
       end,
     })
 
-    vim.keymap.set("n", "<leader>tl", function()
+    vim.keymap.set("n", "<leader>tt", function()
       lint_options.enabled = not lint_options.enabled
       vim.notify(("Linting is %s"):format(lint_options.enabled and "enabled" or "disabled"))
     end)
