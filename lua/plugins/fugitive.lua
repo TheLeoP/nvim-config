5d9367700dc200d1c1af83924f95c7cc6351c685
TheLeoP <eugenio2305@hotmail.com>
2024-05-29 16:29:42 -0500

    Install rhubarb and add :Browse command to support :GBrowse without netrw

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,11 @@
 return {
   "tpope/vim-fugitive",
+  config = function()
+    vim.api.nvim_create_user_command("Browse", function(args) vim.ui.open(args.args) end, {
+      desc = "Enables using GBrowse without netrw",
+      force = true,
+      nargs = 1,
+    })
+  end,
+  dependencies = { "tpope/vim-rhubarb" },
 }
