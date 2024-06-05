6ca9bfc1d500f6e5ed58bb17227543da2895d05e
TheLeoP <eugenio2305@hotmail.com>
2024-06-05 08:56:41 -0500

    Update lazydev config and install vim-cucumber

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,10 +4,19 @@ return {
     cmp = false,
     library = {
       { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "wezterm-types", mods = { "wezterm" } },
+    },
+    integrations = {
+      lspconfig = true,
+      cmp = false,
+      coq = true,
     },
     enabled = function(root_dir)
       return not vim.uv.fs_stat(root_dir .. "/.luarc.json") and not vim.uv.fs_stat(root_dir .. "/.luarc.jsonc")
     end,
   },
-  dependencies = { "Bilal2453/luvit-meta" },
+  dependencies = {
+    "Bilal2453/luvit-meta",
+    "justinsgithub/wezterm-types",
+  },
 }
