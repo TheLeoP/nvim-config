c9cce915201bee4484cfe85f00ba41551418c59e
TheLeoP <eugenio2305@hotmail.com>
2024-06-13 23:34:55 -0500

    Add MiniNotify.clear to ctrl-l mapping

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -10,6 +10,7 @@ return {
       pcall(vim.cmd.nohlsearch)
       pcall(vim.cmd.diffupdate)
       pcall(require("notify").dismiss, { silent = true, pending = true })
+      pcall(require("mini.notify").clear)
       pcall(vim.cmd.normal, { "\12", bang = true }) --[[ ctrl-l]]
     end)
   end,
