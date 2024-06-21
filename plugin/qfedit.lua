dbb1fbcbccc6de77a39e29ed78900a524f4c59c5
TheLeoP <eugenio2305@hotmail.com>
2024-06-20 22:39:37 -0500

    << and >> for navigating qflist

--- plugin/qfedit.lua
+++ plugin/qfedit.lua
@@ -4,3 +4,6 @@ api.nvim_create_autocmd("BufReadPost", {
   callback = function(opts) require("personal.qfedit").start(opts.buf) end,
   group = api.nvim_create_augroup("qfedit-plugin", {}),
 })
+
+vim.keymap.set("n", "<<", function() pcall(vim.cmd.colder) end)
+vim.keymap.set("n", ">>", function() pcall(vim.cmd.cnewer) end)
