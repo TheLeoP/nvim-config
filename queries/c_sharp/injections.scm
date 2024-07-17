8bfc9ca0aa9eb6d1377299fbfc73260acc4e1c9b
TheLeoP <eugenio2305@hotmail.com>
2024-07-17 16:06:41 -0500

    Add injections for sql commands on NpgsqlCommand constructor on c#

---
+++ queries/c_sharp/injections.scm
@@ -0,0 +1,33 @@
+;;extends
+
+; new NpgsqlCommand("");
+(object_creation_expression
+  type: (identifier) @class
+  arguments: (argument_list
+               (argument
+                 (string_literal
+                   (string_literal_content) @injection.content)))
+  (#any-of? @class "NpgsqlCommand")
+  (#set! injection.language "sql"))
+
+; new NpgsqlCommand($"");
+; new NpgsqlCommand(@$"");
+(object_creation_expression
+  type: (identifier) @class
+  arguments: (argument_list
+               (argument
+                 (interpolated_string_expression
+                   (string_content) @injection.content)))
+  (#any-of? @class "NpgsqlCommand")
+  (#set! injection.language "sql")
+  (#set! injection.combined))
+
+; new NpgsqlCommand(@"");
+(object_creation_expression
+  type: (identifier) @class
+  arguments: (argument_list
+               (argument
+                 (verbatim_string_literal) @injection.content))
+  (#any-of? @class "NpgsqlCommand")
+  (#offset! @injection.content 0 2 0 -1)
+  (#set! injection.language "sql"))
