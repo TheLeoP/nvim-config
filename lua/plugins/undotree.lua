5eaafe5419b5656270921bd9ce6a93f9e19fe223
TheLeoP <eugenio2305@hotmail.com>
2024-07-18 12:31:25 -0500

    (undotree) use the `diff` vendored with git on windows

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
   "mbbill/undotree",
   init = function()
-    if vim.fn.has "win32" == 1 then vim.g.undotree_DiffCommand = "FC" end
+    if vim.fn.has "win32" == 1 then vim.g.undotree_DiffCommand = '"C:/Program Files/Git/usr/bin/diff"' end
   end,
 }
