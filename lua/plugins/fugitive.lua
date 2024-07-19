44c96fe174b12c349b4dfb032fe23b5617aa2fed
TheLeoP <eugenio2305@hotmail.com>
2024-07-18 23:45:18 -0500

    (fugitive) remove custom Browse command
    Fugitive now supports vim.ui.open out-of-the-box

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,12 +1,5 @@
 return {
   "tpope/vim-fugitive",
-  config = function()
-    vim.api.nvim_create_user_command("Browse", function(args) vim.ui.open(args.args) end, {
-      desc = "Enables using GBrowse without netrw",
-      force = true,
-      nargs = 1,
-    })
-    vim.keymap.set("n", "g<cr>", "<cmd>Git<cr>")
-  end,
+  config = function() vim.keymap.set("n", "g<cr>", "<cmd>Git<cr>") end,
   dependencies = { "tpope/vim-rhubarb" },
 }
