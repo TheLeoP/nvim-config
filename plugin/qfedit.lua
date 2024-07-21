d0bfb90830c2f196b2df6e49400e7df404e7c6bf
TheLeoP <eugenio2305@hotmail.com>
2024-07-20 21:54:03 -0500

    (qfedit) remove own plugin because of too much edge cases

--- plugin/qfedit.lua
+++
@@ -1,6 +0,0 @@
-local api = vim.api
-api.nvim_create_autocmd("BufReadPost", {
-  pattern = "quickfix",
-  callback = function(opts) require("personal.qfedit").start(opts.buf) end,
-  group = api.nvim_create_augroup("qfedit-plugin", {}),
-})
