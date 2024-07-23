cf47237af278f312e97de6dc15a50e63c9877521
TheLeoP <eugenio2305@hotmail.com>
2024-07-23 10:55:49 -0500

    Update refactoring keymaps

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -7,13 +7,12 @@ return {
     vim.keymap.set("x", "<leader>af", function() refactoring.refactor "Extract Function To File" end)
     vim.keymap.set("x", "<leader>av", function() refactoring.refactor "Extract Variable" end)
     vim.keymap.set({ "n", "x" }, "<leader>ai", function() refactoring.refactor "Inline Variable" end)
-    vim.keymap.set("n", "<leader>abb", function() refactoring.refactor "Extract Block" end)
-    vim.keymap.set("n", "<leader>abf", function() refactoring.refactor "Extract Block To File" end)
+    vim.keymap.set("n", "<leader>aI", function() refactoring.refactor(115) end)
+
     vim.keymap.set("n", "<leader>pP", function() refactoring.debug.printf { below = false } end)
     vim.keymap.set("n", "<leader>pp", function() refactoring.debug.printf { below = true } end)
     vim.keymap.set({ "x", "n" }, "<leader>pv", function() refactoring.debug.print_var { below = true } end)
     vim.keymap.set({ "x", "n" }, "<leader>pV", function() refactoring.debug.print_var { below = false } end)
     vim.keymap.set("n", "<leader>pc", function() refactoring.debug.cleanup {} end)
-    vim.keymap.set("n", "<leader>aI", function() refactoring.refactor(115) end)
   end,
 }
