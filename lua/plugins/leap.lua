6d129dab18a292be43eed725f5c08197c9a09ca0
TheLeoP <eugenio2305@hotmail.com>
2024-07-23 10:56:18 -0500

    Update leap keymaps
    - use builtin experimental treesitter selection
    - remove test keymaps for <F4> and <F5>

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,54 +1,3 @@
-local api = vim.api
-local ts = vim.treesitter
-
-local function get_ts_nodes()
-  if not pcall(ts.get_parser) then return end
-  local wininfo = vim.fn.getwininfo(api.nvim_get_current_win())[1]
-  -- Get current node, and then its parent nodes recursively.
-  local cur_node = ts.get_node()
-  if not cur_node then return end
-
-  local nodes = { cur_node }
-  local parent = cur_node:parent()
-  while parent do
-    table.insert(nodes, parent)
-    parent = parent:parent()
-  end
-
-  -- Create Leap targets from TS nodes.
-  local targets = {}
-  local startline, startcol ---@type integer, integer
-  local endline, endcol ---@type integer, integer
-  for _, node in ipairs(nodes) do
-    startline, startcol, endline, endcol = node:range() -- (0,0)
-    local startpos = { startline + 1, startcol + 1 }
-    local endpos = { endline + 1, endcol + 1 }
-    -- Add both ends of the node.
-    if startline + 1 >= wininfo.topline then table.insert(targets, { pos = startpos, altpos = endpos }) end
-    if endline + 1 <= wininfo.botline then table.insert(targets, { pos = endpos, altpos = startpos }) end
-  end
-  if #targets >= 1 then return targets end
-end
-
-local function select_node_range(target)
-  local mode = api.nvim_get_mode().mode
-
-  -- Force going back to Normal from Visual mode.
-  if not mode:match "no?" then vim.cmd("normal! " .. mode) end
-  vim.fn.cursor(unpack(target.pos))
-  local v = mode:match "V" and "V" or mode:match "" and "" or "v"
-  vim.cmd("normal! " .. v)
-  vim.fn.cursor(unpack(target.altpos))
-end
-
-local function leap_ts()
-  require("leap").leap {
-    target_windows = { api.nvim_get_current_win() },
-    targets = get_ts_nodes,
-    action = select_node_range,
-  }
-end
-
 return {
   {
     "ggandor/leap.nvim",
@@ -68,13 +17,7 @@ return {
         vim.cmd.normal "v" -- simulates backward inclusive_op because it's broken on leap.nvim
         leap.leap { backward = true, offset = 2 }
       end)
-      vim.keymap.set(
-        { "x", "o" },
-        "<F4>",
-        function() leap.leap { backward = true, offset = 0, inclusive_op = true } end
-      )
-      vim.keymap.set({ "x", "o" }, "<F5>", function() leap.leap { backward = true, offset = 0 } end)
-      vim.keymap.set({ "x", "o" }, "gs", leap_ts)
+      vim.keymap.set({ "n", "x", "o" }, "gs", function() require("leap.treesitter").select() end)
 
       vim.keymap.set({ "x", "o" }, "ir", function()
         local ok, char = pcall(vim.fn.getcharstr)
