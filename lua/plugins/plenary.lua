5091776b93fa1df16f3feb738a170e27bbaed6d1
TheLeoP <eugenio2305@hotmail.com>
2024-07-26 23:40:28 -0500

    Remove autocmd for creating keymap to run plenary tests

--- lua/plugins/plenary.lua
+++ lua/plugins/plenary.lua
@@ -1,13 +1,3 @@
 return {
   "nvim-lua/plenary.nvim",
-  config = function()
-    vim.api.nvim_create_autocmd("BufRead", {
-      group = vim.api.nvim_create_augroup("Plenary test", { clear = true }),
-      pattern = "*_spec.lua",
-      callback = function(opts)
-        local bufnr = opts.buf ---@type integer
-        vim.keymap.set("n", "t", "<Plug>PlenaryTestFile", { buffer = bufnr })
-      end,
-    })
-  end,
 }
