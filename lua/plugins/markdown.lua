7b315d532590b744eceaddbd19d780045e2a806a
TheLeoP <eugenio2305@hotmail.com>
2024-07-26 22:49:58 -0500

    Install markdown.nvim for better in-editor rendering

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  opts = {
+    file_types = { "markdown", "rmd" },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+  },
+}
