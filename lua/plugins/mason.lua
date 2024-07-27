e9023bac1eb20524cfaf0bfea2e5a813d4d07071
TheLeoP <eugenio2305@hotmail.com>
2024-07-26 23:38:01 -0500

    Handle non-ls binaries on mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,38 +1,28 @@
-local notify_opts = { title = "Ensure installed" }
----@param name string
-local function ensure_installed(name)
-  local Registry = require "mason-registry"
-  local pkg = Registry.get_package(name)
-  if not pkg:is_installed() then
-    vim.notify(("Installing %s"):format(name), vim.log.levels.INFO, notify_opts)
-    pkg:install():once(
-      "closed",
-      vim.schedule_wrap(function()
-        if pkg:is_installed() then
-          vim.notify(("%s was installed"):format(pkg.name), vim.log.levels.INFO, notify_opts)
-        else
-          vim.notify(("failed to install %s"):format(pkg.name), vim.log.levels.ERROR)
-        end
-      end)
-    )
-  end
-end
-
----@type string[]
-local tools = {
-  "eslint_d",
-  "black",
-  "stylua",
-  "prettierd",
-}
-
 return {
   "williamboman/mason.nvim",
   build = ":MasonUpdate",
-  config = function()
-    require("mason").setup {}
-    for _, tool in ipairs(tools) do
-      ensure_installed(tool)
-    end
+  opts = {},
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require "mason-registry"
+
+    mr.refresh(function()
+      for _, tool in ipairs {
+        "black",
+        "stylua",
+        "prettierd",
+        "hadolint",
+        "cpptools",
+        "csharpier",
+        "debugpy",
+        "netcoredbg",
+        "sql-formatter",
+        "java-debug-adapter",
+        "java-test",
+      } do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then p:install() end
+      end
+    end)
   end,
 }
