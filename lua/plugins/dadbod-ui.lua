7c9e17dac7b16294d4d49c856b0adf5acbdfab3f
TheLeoP <eugenio2305@hotmail.com>
2024-07-26 23:50:24 -0500

    (DBUI) Follow lazyvim settings

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -1,8 +1,18 @@
 return {
   "kristijanhusak/vim-dadbod-ui",
   init = function()
-    vim.g.db_ui_force_echo_notifications = 1
+    local data_path = vim.fn.stdpath "data"
+    vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+    vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+
+    vim.g.db_ui_auto_execute_table_helpers = 1
+
     vim.g.db_ui_show_database_icon = 1
+    vim.g.db_ui_use_nerd_fonts = 1
+
+    vim.g.db_ui_use_nvim_notify = 1
+
+    vim.g.db_ui_execute_on_save = false
   end,
   dependencies = { "vim-dadbod" },
 }
