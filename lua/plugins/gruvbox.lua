f72bc4d0ddc0557ad0bd39839ecadb5b6f71b8a9
TheLeoP <eugenio2305@hotmail.com>
2024-08-12 16:36:37 -0500

    (gruvbox) update highlights for dapui controls
    Avoids incorrect color when focussing vs not focusing dap-rep window

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -28,6 +28,21 @@ return {
         -- fzf-lua uses this by default
         TelescopeSelection = { link = "Visual" },
 
+        DapUIStop = { fg = "#fb4934", bg = "#282828" },
+        DapUIStopNC = { fg = "#fb4934", bg = "#3c3836" },
+        DapUIPlayPause = { fg = "#b8bb26", bg = "#282828" },
+        DapUIPlayPauseNC = { fg = "#b8bb26", bg = "#3c3836" },
+        DapUIRestart = { fg = "#b8bb26", bg = "#282828" },
+        DapUIRestartNC = { fg = "#b8bb26", bg = "#3c3836" },
+        DapUIStepOver = { fg = "#83a598", bg = "#282828" },
+        DapUIStepOverNC = { fg = "#83a598", bg = "#3c3836" },
+        DapUIStepInto = { fg = "#83a598", bg = "#282828" },
+        DapUIStepIntoNC = { fg = "#83a598", bg = "#3c3836" },
+        DapUIStepBack = { fg = "#83a598", bg = "#282828" },
+        DapUIStepBackNC = { fg = "#83a598", bg = "#3c3836" },
+        DapUIStepOut = { fg = "#83a598", bg = "#282828" },
+        DapUIStepOutNC = { fg = "#83a598", bg = "#3c3836" },
+
         ["@lsp.type.interface"] = { link = "@type" },
         ["@lsp.type.struct"] = { link = "@structure" },
 
