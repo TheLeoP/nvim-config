01f1596de3126b470ce949aa717e0c69553a9b3f
TheLeoP <eugenio2305@hotmail.com>
2024-10-09 10:21:35 -0500

    (mini.notify) Make lsp notifications appear and bottom right corner

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -123,6 +123,11 @@ return {
         duration_last = 500,
       },
       window = {
+        config = function()
+          local has_statusline = vim.o.laststatus > 0
+          local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+          return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
+        end,
         max_width_share = 0.25,
       },
     }
