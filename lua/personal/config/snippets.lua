41d15c1d9df38a5a7fda358f1ebbf654836e8bd0
TheLeoP <eugenio2305@hotmail.com>
2024-10-11 23:37:36 -0500

    (snippets) `line_ends` -> `line_end`

--- lua/personal/config/snippets.lua
+++ lua/personal/config/snippets.lua
@@ -170,7 +170,7 @@ elseif <condition> then
     )
   ),
   s(
-    { trig = "fn ", snippetType = "autosnippet", condition = conds.line_begin * conds.line_ends },
+    { trig = "fn ", snippetType = "autosnippet", condition = conds.line_begin * conds.line_end },
     fmta(
       [[
 <visibility>function <name>(<args>)
