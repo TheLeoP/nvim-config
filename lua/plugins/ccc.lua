d52981abfb67f1d9dba751cc6c18ff55a797e6f1
TheLeoP <eugenio2305@hotmail.com>
2024-10-18 17:48:59 -0500

    (colorizer) remove in favor of ccc

--- lua/plugins/ccc.lua
+++ lua/plugins/ccc.lua
@@ -1,4 +1,20 @@
 return {
   "uga-rosa/ccc.nvim",
-  opts = {},
+  opts = {
+    highlighter = {
+      auto_enabled = true,
+      filetypes = {
+        "css",
+        "javascript",
+        "typescript",
+        "typescriptreact",
+        "javascriptreact",
+        "vue",
+        "html",
+        "dbout",
+        "sql",
+        "lua",
+      },
+    },
+  },
 }
