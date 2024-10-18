aac40d99366977352d642e676bfd4abf23f64619
TheLeoP <eugenio2305@hotmail.com>
2024-10-18 13:23:31 -0500

    (srr) Add ssr plugin (structural search and replace)

---
+++ lua/plugins/ssr.lua
@@ -0,0 +1,26 @@
+return {
+  "cshuaimin/ssr.nvim",
+  module = "ssr",
+  opts = function()
+    local factor = 0.9
+    local max_height = math.floor(vim.o.lines * factor)
+    return {
+      border = "rounded",
+      max_height = max_height,
+      adjust_window = true,
+      -- these are mostly defautls but are here for reference
+      keymaps = {
+        close = "q",
+        next_match = "n",
+        prev_match = "N",
+        replace_confirm = "<cr>",
+        replace_all = "<c-cr>",
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("ssr").setup(opts)
+
+    vim.keymap.set({ "n", "x" }, "<leader>l", function() require("ssr").open() end)
+  end,
+}
