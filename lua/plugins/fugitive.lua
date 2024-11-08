c74fcd9db8a759301dc4de01c6394fbc1e06e5e8
TheLeoP <eugenio2305@hotmail.com>
2024-11-08 11:46:19 -0500

    (fugitive) add git log keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "g<cr>", "<cmd>Git<cr>")
+    vim.keymap.set("n", "gl", "<cmd>Git log -50 --oneline<cr>")
     vim.keymap.set("n", "g<space>", ":Git ")
   end,
   dependencies = { "tpope/vim-rhubarb" },
