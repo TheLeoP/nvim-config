7c217d79a85aa8b199972399b1a71a5da921ce80
TheLeoP <eugenio2305@hotmail.com>
2024-11-26 19:52:04 -0500

    (bujo) cb -> coroutine

--- lua/personal/bujo.lua
+++ lua/personal/bujo.lua
@@ -1,6 +1,7 @@
 -- ported from https://github.com/vuciv/vim-bujo
 
 local fs_exists = require("personal.util.general").fs_exists
+local co_resume = require("personal.auv").co_resume
 
 local M = {}
 
@@ -17,76 +18,74 @@ coroutine.wrap(function()
   if not exists then vim.fn.mkdir(data_path) end
 end)()
 
----@param cb fun(is: boolean)
-local function is_git_repo(cb)
+---@async
+---@return boolean is
+local function is_git_repo()
+  local co = coroutine.running()
   vim.system({ "git", "rev-parse", "--is-inside-work-tree" }, { text = true }, function(result)
     if result.stderr ~= "" then
-      cb(false)
+      co_resume(co, false)
       return
     end
 
     local out = vim.split(result.stdout, "\n")[1]
     if out ~= "true" then
-      cb(false)
+      co_resume(co, false)
       return
     end
-
-    cb(true)
+    co_resume(co, true)
   end)
+  return coroutine.yield()
 end
 
----@param cb fun(name: string|nil)
-local function repo_name(cb)
+---@async
+---@return string|nil name
+local function repo_name()
+  local co = coroutine.running()
+
   vim.system({ "git", "rev-parse", "--show-toplevel" }, { text = true }, function(result)
     if result.stderr ~= "" then
       vim.notify(result.stderr, vim.log.levels.ERROR)
-      cb()
+      co_resume(co)
       return
     end
 
     if result.stderr ~= "" then
       vim.notify(result.stderr, vim.log.levels.ERROR)
-      cb()
+      co_resume(co)
       return
     end
     local out = vim.split(result.stdout, "\n")[1]
     local segments = vim.split(out, "/")
     local name = segments[#segments]
-    cb(name)
+    co_resume(co, name)
   end)
+  return coroutine.yield()
 end
 
----@param cb fun(path: string)
-local function get_path(cb)
-  is_git_repo(function(is)
-    if not is then
-      cb(("%s/todo.md"):format(data_path))
-      return
-    end
-    repo_name(function(name)
-      if name == nil then return end
-      local todo_path = ("%s/%s"):format(data_path, name)
-      coroutine.wrap(function()
-        local exists, err = fs_exists(todo_path)
-        if err then return vim.notify(err, vim.log.levels.ERROR) end
-        if exists == nil then return end
-        if not exists then vim.fn.mkdir(todo_path) end
-        cb(("%s/todo.md"):format(todo_path))
-      end)()
-    end)
-  end)
+---@async
+---@return string|nil path
+local function get_path()
+  if not is_git_repo() then return ("%s/todo.md"):format(data_path) end
+  if repo_name() == nil then return ("%s/todo.md"):format(data_path) end
+  local todo_path = ("%s/%s"):format(data_path, repo_name())
+  local exists, err = fs_exists(todo_path)
+  if err then return vim.notify(err, vim.log.levels.ERROR) end
+  if exists == nil then return end
+  if not exists then vim.fn.mkdir(todo_path) end
+  return ("%s/todo.md"):format(todo_path)
 end
 
 ---@param width? integer
 function M.open(width)
-  get_path(vim.schedule_wrap(
-    function(path)
-      vim.cmd.vsplit {
-        range = { width or default_width },
-        args = { path },
-        mods = { split = "aboveleft" },
-      }
-    end
-  ))
+  coroutine.wrap(function()
+    local path = get_path()
+    if not path then return end
+    vim.cmd.vsplit {
+      range = { width or default_width },
+      args = { path },
+      mods = { split = "aboveleft" },
+    }
+  end)()
 end
 return M
