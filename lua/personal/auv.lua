6b695a0ba0ccc5d882c710ff025f57c349ac877e
TheLeoP <eugenio2305@hotmail.com>
2024-11-26 19:51:38 -0500

    (auv) make all wrappers vim.schedule_wrapped

--- lua/personal/auv.lua
+++ lua/personal/auv.lua
@@ -1,5 +1,7 @@
 local uv = vim.uv
 
+-- TODO: maybe refactor this and dependencies to not be schedule_wrapped
+--- Coroutine friendly and vim.schedule_wrapped libuv wrappers
 local M = {}
 
 ---@param co thread
@@ -15,7 +17,7 @@ end
 ---@return nil|string err, integer|nil fd
 function M.fs_open(path, flags, mode)
   local co = coroutine.running()
-  uv.fs_open(path, flags, mode, function(err, fd) M.co_resume(co, err, fd) end)
+  uv.fs_open(path, flags, mode, vim.schedule_wrap(function(err, fd) M.co_resume(co, err, fd) end))
   return coroutine.yield()
 end
 
@@ -25,7 +27,7 @@ end
 ---@return nil|string err, string|nil data
 function M.fs_read(fd, size, offset)
   local co = coroutine.running()
-  uv.fs_read(fd, size, offset, function(err, data) M.co_resume(co, err, data) end)
+  uv.fs_read(fd, size, offset, vim.schedule_wrap(function(err, data) M.co_resume(co, err, data) end))
   return coroutine.yield()
 end
 
@@ -33,7 +35,7 @@ end
 ---@return nil|string err, table|nil stat
 function M.fs_stat(path)
   local co = coroutine.running()
-  uv.fs_stat(path, function(err, stat) M.co_resume(co, err, stat) end)
+  uv.fs_stat(path, vim.schedule_wrap(function(err, stat) M.co_resume(co, err, stat) end))
   return coroutine.yield()
 end
 
@@ -41,7 +43,7 @@ end
 ---@return nil|string err, table|nil stat
 function M.fs_fstat(fd)
   local co = coroutine.running()
-  uv.fs_fstat(fd, function(err, stat) M.co_resume(co, err, stat) end)
+  uv.fs_fstat(fd, vim.schedule_wrap(function(err, stat) M.co_resume(co, err, stat) end))
   return coroutine.yield()
 end
 
@@ -49,7 +51,7 @@ end
 ---@return nil|string err, table|nil stat
 function M.fs_close(fd)
   local co = coroutine.running()
-  uv.fs_close(fd, function(err, success) M.co_resume(co, err, success) end)
+  uv.fs_close(fd, vim.schedule_wrap(function(err, success) M.co_resume(co, err, success) end))
   return coroutine.yield()
 end
 
