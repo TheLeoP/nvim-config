e6f3e3a3e8d40a3e457782d482fabffeaeaf2238
TheLeoP <eugenio2305@hotmail.com>
2024-12-02 18:58:15 -0500

    (ps1) Add $ as part of keywords

--- ftplugin/ps1.lua
+++ ftplugin/ps1.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>P", function() require("powershell").toggle_term() end)
 vim.keymap.set({ "n", "x" }, "<leader>E", function() require("powershell").eval() end)
+
+vim.bo.iskeyword = vim.bo.iskeyword .. ",$"
