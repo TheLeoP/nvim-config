e1543643f24b8a670f1219bd6381fa18ecf74ca8
TheLeoP <eugenio2305@hotmail.com>
2024-12-26 22:38:09 -0500

    (markdown) remove custom bold and italic operators

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,35 +1,5 @@
 require("personal.util.writing").setup()
 
--- Permite crear un *operador* usando el nombre de una función lua.
--- El nombre de la función puede incluir 'require', pero se deben omitir los paréntesis
--- Ejemplo:
---
--- ```lua
--- operatorfunc_lua("require'foo.bar'.something")
--- ```
----@param fn_name string nombre de la función que será el *operador*
----@return function @closure a usar como parámetro en vim.keymap.set
-local operatorfunc_lua = function(fn_name)
-  return function()
-    vim.o.operatorfunc = ("v:lua.%s"):format(fn_name)
-    return "g@"
-  end
-end
-
-vim.keymap.set(
-  "n",
-  "<leader>b",
-  operatorfunc_lua "require'personal.util.markdown'.bold",
-  { buffer = true, expr = true }
-)
-
-vim.keymap.set(
-  "n",
-  "<leader>i",
-  operatorfunc_lua "require'personal.util.markdown'.italic",
-  { buffer = true, expr = true }
-)
-
 -- <c-bs>
 vim.keymap.set("n", "\08", function()
   local cur_line = vim.api.nvim_get_current_line()
