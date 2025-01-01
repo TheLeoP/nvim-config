d091a403714db2d8013d2994b6da5c667f58e3d0
TheLeoP <eugenio2305@hotmail.com>
2024-12-31 21:06:38 -0500

    (matchup) disable mouse

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -4,5 +4,6 @@ return {
     vim.g.matchup_delim_noskips = 1 -- recognize only symbols in strings and comments
     vim.g.matchup_matchparen_offscreen = {} -- disable feature
     vim.g.matchup_matchparen_deferred = 1
+    vim.g.matchup_mouse_enabled = 0
   end,
 }
