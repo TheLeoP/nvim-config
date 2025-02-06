fc79d5eb9fe6992aeb27b6e0efd5ae287324a43d
TheLeoP <eugenio2305@hotmail.com>
2025-02-05 22:29:07 -0500

    (google) move google auth functions to separate module

--- lua/personal/calendar.lua
+++ lua/personal/calendar.lua
@@ -7,31 +7,15 @@ local iter = vim.iter
 local compute_hex_color_group = require("mini.hipatterns").compute_hex_color_group
 local hl_enable = require("mini.hipatterns").enable
 local notify = require "mini.notify"
-local fs_exists = require("personal.util.general").fs_exists
 local new_uid = require("personal.util.general").new_uid
 local url_encode = require("personal.util.general").url_encode
 local auv = require "personal.auv"
 local co_resume = auv.co_resume
+local google = require "personal.google"
+local refresh_access_token = google.refresh_access_token
+local get_token_info = google.get_token_info
 
 local M = {}
-
-local api_key = vim.env.GOOGLE_CALENDAR_API_KEY ---@type string
-local client_id = vim.env.GOOGLE_CALENDAR_CLIENT_ID ---@type string
-local client_secret = vim.env.GOOGLE_CALENDAR_CLIENT_SECRET ---@type string
-
-local data_path = ("%s/%s"):format(vim.fn.stdpath "data", "/calendar")
-data_path = vim.fs.normalize(data_path)
-coroutine.wrap(function()
-  local exists, err = fs_exists(data_path)
-  if exists == nil and err then
-    vim.notify(err, vim.log.levels.ERROR)
-    return
-  end
-  if not exists then
-    auv.schedule()
-    vim.fn.mkdir(data_path)
-  end
-end)()
 
 ---@type string, string, string
 local timezone_sign, timezone_offset_hours_s, timezone_offset_minutes_s =
@@ -41,245 +25,6 @@ local timezone_offset_hours = (timezone_sign == "-" and -1 or 1) * tonumber(time
 local timezone_offset_minutes = (timezone_sign == "-" and -1 or 1) * tonumber(timezone_offset_minutes_s)
 -- TODO: hardcoded timezone
 local text_timezone = "America/Guayaquil"
-
----@param opts {on_ready: fun(), on_code: fun(code: string)}
-local function start_server(opts)
-  local server = assert(uv.new_tcp())
-  local buffer = {} ---@type string[]
-
-  server:bind("127.0.0.1", 8080)
-  server:listen(180, function(err)
-    assert(not err, err)
-    local client = uv.new_tcp()
-    assert(server:accept(client))
-    assert(client:read_start(vim.schedule_wrap(
-      ---@param err2 string|nil
-      ---@param data string|nil
-      function(err2, data)
-        assert(not err2, err2)
-        if data then table.insert(buffer, data) end
-        if data and data:find "\r\n\r\n$" then
-          local request = table.concat(buffer, "")
-          local lines = vim.split(request, "\r\n", { trimempty = true })
-          local code = lines[1]:match "code=([^&]+)&"
-          local err3 = lines[1]:match "error=([^&]+)&"
-          assert(not err3, err3)
-          assert(uv.write(
-            client,
-            ([[HTTP/1.1 200 OK
-Content-Type: text/html
-
-<!DOCTYPE html>
-<html>
-  <body>
-    <h1>Everything done, you can close this and return to Neovim :D</h1>
-  </body>
-</html>
-]]):format(code)
-          ))
-          assert(uv.read_stop(client))
-          uv.close(client)
-          uv.close(server)
-
-          opts.on_code(code)
-        end
-      end
-    )))
-  end)
-  opts.on_ready()
-end
-
-local token_url = "https://oauth2.googleapis.com/token"
-local refresh_token_path = ("%s/refresh_token.json"):format(data_path)
-local _cache_token_info ---@type TokenInfo|nil
-local is_refreshing_access_token = false
-
----@async
----@param refresh_token string
----@return TokenInfo
-local function refresh_access_token(refresh_token)
-  local co = coroutine.running()
-  assert(co, "The function must run inside a coroutine")
-
-  local token_refreshed_pattern = "CalendarAccessTokenRefreshed"
-  api.nvim_create_autocmd("User", {
-    pattern = token_refreshed_pattern,
-    ---@param opts {data:{token_info: TokenInfo}}
-    callback = function(opts) co_resume(co, opts.data.token_info) end,
-    once = true,
-  })
-  if is_refreshing_access_token then return coroutine.yield() end
-  is_refreshing_access_token = true
-
-  local params = ("client_id=%s&client_secret=%s&grant_type=refresh_token&refresh_token=%s"):format(
-    client_id,
-    client_secret,
-    refresh_token
-  )
-  vim.system(
-    {
-      "curl",
-      "--data",
-      params,
-      "--http1.1",
-      "--silent",
-      token_url,
-    },
-    { text = true },
-    vim.schedule_wrap(function(result)
-      assert(result.stderr == "", result.stderr)
-      local ok, new_token_info = pcall(vim.json.decode, result.stdout) ---@type boolean, NewTokenInfo|ApiTokenErrorResponse|string
-      assert(ok, new_token_info)
-      ---@cast new_token_info -string
-
-      if new_token_info.error then
-        ---@cast new_token_info -NewTokenInfo
-        assert(new_token_info.error == "invalid_grant", vim.inspect(new_token_info))
-
-        _cache_token_info = nil
-        assert(vim.fn.delete(refresh_token_path) == 0, ("Couldn't delete file %s"):format(refresh_token_path))
-
-        coroutine.wrap(function()
-          local token_info = M.get_token_info()
-          assert(token_info, "There is no token_info")
-          is_refreshing_access_token = false
-          vim.schedule(
-            function()
-              api.nvim_exec_autocmds("User", { pattern = token_refreshed_pattern, data = { token_info = token_info } })
-            end
-          )
-        end)()
-
-        return
-      end
-      ---@cast new_token_info +NewTokenInfo
-      ---@cast new_token_info -ApiTokenErrorResponse
-
-      _cache_token_info.access_token = new_token_info.access_token
-      _cache_token_info.expires_in = new_token_info.expires_in
-      _cache_token_info.scope = new_token_info.scope
-      _cache_token_info.token_type = new_token_info.token_type
-
-      local file = io.open(refresh_token_path, "w")
-      assert(file)
-      local ok2, token_info_string = pcall(vim.json.encode, _cache_token_info) ---@type boolean, string
-      assert(ok2, token_info_string)
-      file:write(token_info_string)
-      file:close()
-
-      is_refreshing_access_token = false
-      api.nvim_exec_autocmds("User", { pattern = token_refreshed_pattern, data = { token_info = _cache_token_info } })
-    end)
-  )
-  return coroutine.yield()
-end
-
-local scope = "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/tasks"
-scope = assert(url_encode(scope))
-local redirect_uri = "http://localhost:8080"
-redirect_uri = assert(url_encode(redirect_uri))
-
-local auth_url = "https://accounts.google.com/o/oauth2/auth"
-local full_auth_url = ("%s?client_id=%s&redirect_uri=%s&scope=%s&response_type=code"):format(
-  auth_url,
-  client_id,
-  redirect_uri,
-  scope
-)
-
----@class TokenInfo
----@field access_token string
----@field expires_in integer
----@field refresh_token string
----@field scope string
----@field token_type string
-
----@class NewTokenInfo
----@field access_token string
----@field expires_in integer
----@field scope string
----@field token_type string
-
----Reads from file if exists and asks for a token if not. May return an invalid/revoked token
----@async
----@return TokenInfo|nil
-function M.get_token_info()
-  local co = coroutine.running()
-  assert(co, "The function must run inside a coroutine")
-
-  if _cache_token_info then return _cache_token_info end
-
-  local exists, err = fs_exists(refresh_token_path)
-  if exists == nil and err then
-    vim.notify(err, vim.log.levels.ERROR)
-    return
-  end
-  if exists then
-    local fd
-    err, fd = auv.fs_open(refresh_token_path, "r", 292) ---444
-    if err then return vim.notify(err, vim.log.levels.ERROR) end
-    ---@cast fd -nil
-    local stat
-    err, stat = auv.fs_fstat(fd)
-    if err then return vim.notify(err, vim.log.level.ERROR) end
-    ---@cast stat -nil
-    local content ---@type string|nil
-    err, content = auv.fs_read(fd, stat.size, 0)
-    if err then return vim.notify(err, vim.log.level.ERROR) end
-    ---@cast content -nil
-    err = auv.fs_close(fd)
-    if err then return vim.notify(err, vim.log.level.ERROR) end
-
-    local ok, token_info = pcall(vim.json.decode, content) ---@type boolean, string|TokenInfo
-    assert(ok, token_info)
-    ---@cast token_info -string
-
-    _cache_token_info = token_info
-    return token_info
-  end
-
-  vim.notify "You need to give Calendar access to your google account"
-  start_server {
-    on_ready = function()
-      vim.ui.open(full_auth_url)
-      vim.notify "A webpage asking for access to your google accoutn has been opened"
-    end,
-    on_code = function(code)
-      local params = ("client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s"):format(
-        client_id,
-        client_secret,
-        code,
-        redirect_uri
-      )
-
-      vim.system({
-        "curl",
-        "--data",
-        params,
-        "--http1.1",
-        "--silent",
-        -- TODO: is this needed?
-        -- "--insecure",
-        token_url,
-      }, { text = true }, function(result)
-        assert(result.stderr == "", result.stderr)
-
-        local ok, token_info = pcall(vim.json.decode, result.stdout) ---@type boolean, string|TokenInfo
-        assert(ok, token_info)
-        ---@cast token_info -string
-
-        local file = io.open(refresh_token_path, "w")
-        assert(file)
-        file:write(result.stdout)
-        file:close()
-
-        _cache_token_info = token_info
-        co_resume(co, token_info)
-      end)
-    end,
-  }
-  return coroutine.yield()
-end
 
 ---@class DefaultReminder
 ---@field method string
@@ -398,7 +143,7 @@ local sep = " | "
 ---@param opts? {refresh:boolean}
 function M.calendar_list_show(opts)
   coroutine.wrap(function()
-    local token_info = M.get_token_info()
+    local token_info = get_token_info()
     assert(token_info, "There is no token_info")
     local calendar_list = M.get_calendar_list(token_info, opts)
 
@@ -527,7 +272,7 @@ function M.calendar_list_show(opts)
             if diff.type == "new" then
               assert(diff.summary, ("Diff has no summary %s"):format(vim.inspect(diff)))
               coroutine.wrap(function()
-                token_info = M.get_token_info()
+                token_info = get_token_info()
                 assert(token_info, "There is no token_info")
                 local new_calendar = M.create_calendar(token_info, diff)
                 assert(_cache_calendar_list)
@@ -537,7 +282,7 @@ function M.calendar_list_show(opts)
               end)()
             elseif diff.type == "edit" then
               coroutine.wrap(function()
-                token_info = M.get_token_info()
+                token_info = get_token_info()
                 assert(token_info, "There is no token_info")
                 local edited_calendar = M.edit_calendar(token_info, diff)
                 local cached_calendar = diff.cached_calendar --[[@as table<unknown, unknown>]]
@@ -553,7 +298,7 @@ function M.calendar_list_show(opts)
               end)()
             elseif diff.type == "delete" then
               coroutine.wrap(function()
-                token_info = M.get_token_info()
+                token_info = get_token_info()
                 assert(token_info, "There is no token_info")
                 M.delete_calendar(token_info, diff)
                 assert(_cache_calendar_list)
@@ -844,7 +589,7 @@ end
 ---@param id string
 function M.calendar_show(id)
   coroutine.wrap(function()
-    local token_info = M.get_token_info()
+    local token_info = get_token_info()
     assert(token_info, "There is no token_info")
 
     local calendar = M.get_calendar(token_info, id)
@@ -1922,7 +1667,7 @@ function CalendarView:show(year, month, opts)
     os.date("*t", os.time { year = last_date.year, month = last_date.month, day = last_date.day + 1 })
 
   coroutine.wrap(function()
-    local token_info = M.get_token_info()
+    local token_info = get_token_info()
     assert(token_info, "There is no token_info")
     local calendar_list, maybe_new_token_info = M.get_calendar_list(token_info, {})
     token_info = maybe_new_token_info or token_info
@@ -2718,7 +2463,7 @@ function M.add_coq_completion()
 
       if sep_num == 1 or sep_num == 3 then
         coroutine.wrap(function()
-          local token_info = M.get_token_info()
+          local token_info = get_token_info()
           assert(token_info, "There is no token_info")
           local calendar_list = M.get_calendar_list(token_info, {})
 
