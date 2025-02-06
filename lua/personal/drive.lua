832dc585705e81b9ecb7278ce2b5a38b94326902
TheLeoP <eugenio2305@hotmail.com>
2025-02-06 16:29:20 -0500

    (drive) add small google drive script to auto upload files

---
+++ lua/personal/drive.lua
@@ -0,0 +1,143 @@
+local auv = require "personal.auv"
+local co_resume = auv.co_resume
+local google = require "personal.google"
+local refresh_access_token = google.refresh_access_token
+local get_token_info = google.get_token_info
+local fs = vim.fs
+
+---@class FileResponse
+---@field id string
+---@field kind "drive#file"
+---@field mimeType string
+---@field name string
+
+---@param token_info TokenInfo
+---@param image_name string
+---@param name string?
+---@return FileResponse?
+local function upload_screenshot(token_info, image_name, name)
+  local co = coroutine.running()
+
+  if not name then
+    vim.ui.input({ prompt = "name", default = "task2_step1_.png" }, function(input) co_resume(co, input) end)
+    name = coroutine.yield() ---@type string|nil
+    if not name then return end
+  end
+
+  local image_file = io.open(image_name, "r")
+  assert(image_file)
+  local image = image_file:read "*a"
+  image_file:close()
+
+  local tmp_name = os.tmpname()
+  local tmp_file = io.open(tmp_name, "w")
+  assert(tmp_file)
+  local data = ([[--foo
+Content-Type: application/json; charset=UTF-8
+
+%s
+
+--foo
+Content-Type: image/png
+
+%s
+--foo--
+]]):format(
+    vim.json.encode {
+      name = name,
+      parents = { "1XdSAONKCW96NfsTOZYNFLEA8oNxAyZIL" },
+    },
+    image
+  )
+  tmp_file:write(data)
+  tmp_file:close()
+
+  vim.system({
+    "curl",
+    "--data-binary",
+    ("@%s"):format(tmp_name),
+    "--http1.1",
+    "--silent",
+    "--header",
+    ("Authorization: Bearer %s"):format(token_info.access_token),
+    "--header",
+    "Content-type: multipart/related; boundary=foo",
+    "--header",
+    ("Content-Length: %d"):format(#data),
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+  }, { text = true }, function(result)
+    assert(result.stderr == "", result.stderr)
+
+    local ok, response = pcall(vim.json.decode, result.stdout) ---@type boolean, string|FileResponse|ApiErrorResponse
+    assert(ok, response)
+    ---@cast response -string
+
+    if response.error then
+      ---@cast response -FileResponse
+      assert(response.error.status == "UNAUTHENTICATED", response.error.message)
+      coroutine.wrap(function()
+        auv.schedule()
+        local refreshed_token_info = refresh_access_token(token_info.refresh_token)
+        local file_response = upload_screenshot(refreshed_token_info, image_name, name)
+        co_resume(co, file_response)
+      end)()
+
+      return
+    end
+
+    co_resume(co, response)
+  end)
+
+  return coroutine.yield()
+end
+
+local already_seen = {} ---@type table<string, boolean>
+
+local w = assert(vim.uv.new_fs_event())
+local path = "/home/luis/Descargas/screenshot"
+local ok = w:start(path, {}, function(err, filename, events)
+  if err then return vim.notify(err, vim.log.levels.ERROR) end
+
+  if not filename then return end
+
+  local fullpath = fs.normalize(fs.joinpath(path, filename))
+
+  coroutine.wrap(function()
+    local type ---@type 'changed' | 'created' | 'deleted'
+
+    if events.rename then
+      local staterr, _ = auv.fs_stat(fullpath)
+      if staterr and staterr:find "^ENOENT:" then
+        type = "deleted"
+      else
+        assert(not staterr, staterr)
+        type = "created"
+      end
+    elseif events.change then
+      type = "changed"
+    end
+    assert(type)
+
+    if type ~= "created" or not fullpath:find "%.png$" or already_seen[fullpath] then return end
+    already_seen[fullpath] = true
+
+    local token_info = get_token_info()
+    assert(token_info, "There is no token_info")
+    auv.schedule()
+    local file_response = upload_screenshot(token_info, fullpath)
+    if not file_response then return end
+    -- TODO: test if this may not work in some case (i.e. if I need to get the link in a different way)
+    auv.schedule()
+    vim.fn.setreg("+", ("https://drive.google.com/file/d/%s/view?usp=drive_link"):format(file_response.id))
+    vim.system { "dunstify", "Document link has been put in the clipboard", "--timeout=600" }
+  end)()
+end)
+if ok ~= 0 then return vim.notify "File watcher couldn't be started" end
+vim.notify(("File watcher for dir `%s` was started"):format(path))
+
+vim.keymap.set("n", "<F4>", function()
+  local ok2 = w:stop()
+  if ok2 ~= 0 then return vim.notify "File watcher couldn't be stopped" end
+  vim.notify "File watcher removed"
+  vim.keymap.del("n", "<F4>")
+end)
