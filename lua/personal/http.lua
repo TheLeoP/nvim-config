895eb05d2af58c65b16ceab14d0898fda5ca5cfe
TheLeoP <eugenio2305@hotmail.com>
2025-02-12 17:45:22 -0500

    (http) add HTTP message parser

--- lua/personal/http.lua
+++ lua/personal/http.lua
@@ -56,34 +56,122 @@ local path_rootless = segment_nz * (P "/" * segment) ^ 0
 local path_noscheme = segment_nz_nc * (P "/" * segment) ^ 0
 local path_absolute = P "/" * (segment_nz * (P "/" * segment) ^ 0) ^ -1
 local path_abempty = (P "/" * segment) ^ 0
--- local _path = path_abempty + path_absolute + path_noscheme + path_rootless + path_empty
+
+local query = Cg((pchar + P "/" + P "?") ^ 0, "query")
+local host = Cg(ip_literal + ipv4adress + reg_name, "host")
+local port = Cg(digit ^ 0 / tonumber, "port")
+
+local scheme = Cg(alpha * (alnum + S "+-.") ^ 0, "scheme")
+local userinfo = Cg((unreserved + pct_encoded + sub_delims + P ":") ^ 0, "userinfo")
+local authority = Ct((userinfo * P "@") ^ -1 * host * (P ":" * port) ^ -1)
+local hier_part = Cg(
+  Ct(
+    P "//" * Cg(authority, "authority") * Cg(path_abempty, "path")
+      + Cg(path_absolute, "path")
+      + Cg(path_rootless, "path")
+      + Cg(path_noscheme, "path")
+      + Cg(path_empty, "path")
+  ),
+  "hier_part"
+)
 
 -- local trace = require("personal.pegdebug").trace
 ---@diagnostic disable-next-line: missing-fields
 local uri_grammar = P {
   "uri",
-  scheme = Cg(alpha * (alnum + S "+-.") ^ 0, "scheme"),
-  userinfo = Cg((unreserved + pct_encoded + sub_delims + P ":") ^ 0, "userinfo"),
-  host = Cg(ip_literal + ipv4adress + reg_name, "host"),
-  port = Cg(digit ^ 0 / tonumber, "port"),
-  authority = Ct((V "userinfo" * P "@") ^ -1 * V "host" * (P ":" * V "port") ^ -1),
-  hier_part = Cg(
-    Ct(
-      P "//" * Cg(V "authority", "authority") * Cg(path_abempty, "path")
-        + Cg(path_absolute, "path")
-        + Cg(path_rootless, "path")
-        + Cg(path_noscheme, "path")
-        + Cg(path_empty, "path")
-    ),
-    "hier_part"
-  ),
-  query = Cg((pchar + P "/" + P "?") ^ 0, "query"),
+  scheme = scheme,
+  userinfo = userinfo,
+  host = host,
+  port = port,
+  authority = authority,
+  hier_part = hier_part,
+  query = query,
   fragment = Cg((pchar + P "/" + P "?") ^ 0, "fragment"),
   uri = Ct(V "scheme" * P ":" * V "hier_part" * (P "?" * V "query") ^ -1 * (P "#" * V "fragment") ^ -1 * -P(1)),
 }
 
+local crlf = P "\r\n"
+local sp = P " "
+local htab = P "\t"
+
+local tchar = S "!#$%&'*+-.^_`|~" + digit + alpha
+local token = tchar ^ 1
+
+local absolute_path = (P "/" * segment) ^ 1
+local absolute_URI = scheme * P ":" * hier_part * (P "?" * query) ^ -1
+
+local status_code = patcount(digit, 3, 3)
+local vchar = R "!~"
+local obs_text = R "\128\255"
+
+local ows = (sp + htab) ^ 0
+local rws = (sp + htab) ^ 1
+local bws = ows
+
+local field_vchar = vchar + obs_text
+local field_content = field_vchar * ((sp + htab + field_vchar) ^ 1 - ((sp + htab) * crlf)) ^ -1
+local field_value = field_content ^ 0
+
+local field_name = token
+
+local octet = P(1)
+
+-- local trace = require("personal.pegdebug").trace
+---@diagnostic disable-next-line: missing-fields
+local http_grammar = P {
+  "message",
+  message_body = Cg(octet ^ 0, "body"),
+  field_value = field_value,
+  field_line = C(field_name) * P ":" * ows * C(field_value) * ows,
+  reason_phrase = (htab + sp + vchar + obs_text) ^ 1,
+  status_line = Ct(
+    V "http_version" * sp * Cg(status_code / tonumber, "code") * sp * Cg(V "reason_phrase", "status") ^ -1
+  ),
+  http_name = S "Hh" * S "Tt" * S "Tt" * S "Pp",
+  http_version = V "http_name" * P "/" * Cg(digit * P "." * digit, "version"),
+  asterisk_form = P "*",
+  authority_form = host * P ":" * port,
+  absolute_form = absolute_URI,
+  origin_form = absolute_path * (P "?" * query) ^ -1,
+  request_target = V "origin_form" + V "absolute_form" + V "authority_form" + V "asterisk_form",
+  method = token,
+  request_line = Ct(Cg(V "method", "method") * sp * Cg(V "request_target", "target") * sp * V "http_version"),
+  start_line = Cg(V "request_line", "request") + Cg(V "status_line", "status"),
+  message = Ct(
+    V "start_line"
+      * crlf
+      * Cg(Ct "" * ((V "field_line" * crlf) % rawset) ^ 0, "headers")
+      * crlf
+      * V "message_body" ^ -1
+      * P(-1)
+  ),
+}
+
+---@class http.Status
+---@field code integer
+---@field status string
+---@field version string
+
+---@class http.Request
+---@field method string
+---@field target string
+---@field version string
+
+---@class http.Response
+---@field request http.Request
+---@field headers table<string, string>
+---@field body string
+
+---@class http.Authority
+---@field host string
+---@field userinfo string
+
+---@class http.HierPart
+---@field authority http.Authority
+---@field path string
+
 ---@class http.Uri
----@field hier_part {authority: {host: string, userinfo: string}, path: string}
+---@field hier_part http.HierPart
 ---@field port integer
 ---@field query string
 ---@field fragment string
@@ -91,7 +179,7 @@ local uri_grammar = P {
 
 ---@param uri string
 ---@param headers table<string, string>|nil
----@return string
+---@return http.Response
 local function get(uri, headers)
   -- TODO: handle http vs https
   local co = coroutine.running()
@@ -148,20 +236,15 @@ Connection: close
   end)
   coroutine.yield(co)
 
-  -- TODO: parse response with lpeg
   local response = coroutine.yield() ---@type string
   client:read_stop()
   client:close()
-  return response
+  return http_grammar:match(response)
 end
 
-local a = uri_grammar:match "https://google.com:80"
--- __AUTO_GENERATED_PRINT_VAR_START__
-print([==[ a:]==], vim.inspect(a)) -- __AUTO_GENERATED_PRINT_VAR_END__
-
--- coroutine.wrap(function()
---   local result = get "http://httpbingo.org/get"
---   -- __AUTO_GENERATED_PRINT_VAR_START__
---   print([==[function result:]==], vim.inspect(result)) -- __AUTO_GENERATED_PRINT_VAR_END__
---   --
--- end)()
+coroutine.wrap(function()
+  local result = get("http://httpbingo.org/get", { test = "value" })
+  local body = vim.json.decode(result.body)
+  -- __AUTO_GENERATED_PRINT_VAR_START__
+  print([==[function body:]==], vim.inspect(body)) -- __AUTO_GENERATED_PRINT_VAR_END__
+end)()
