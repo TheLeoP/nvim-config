c8ff877ebabdf9461744dc426563f7a917ef661e
TheLeoP <eugenio2305@hotmail.com>
2025-02-17 13:59:32 -0500

    (tls) initial commit, aes encrypt/decrypt working

---
+++ lua/personal/tls.lua
@@ -0,0 +1,619 @@
+local iter = vim.iter
+local bit = require "bit"
+local rshift, band, bxor = bit.rshift, bit.band, bit.bxor
+
+local M = {}
+
+local function random()
+  local random_list = {} ---@type string[]
+  for _ = 1, 4 do
+    local number = math.random(0, 0xffffffff)
+
+    local byte1 = rshift(number, 24)
+    local byte2 = band(rshift(number, 16), 0x000000ff)
+    local byte3 = band(rshift(number, 8), 0x000000ff)
+    local byte4 = band(number, 0x000000ff)
+
+    table.insert(random_list, string.char(byte1, byte2, byte3, byte4))
+  end
+
+  local random_32_bytes = table.concat(random_list, "")
+  return random_32_bytes
+end
+
+-- Always takes 1 byte (because the enumeration uses numbers up to 255)
+---@enum http.handshake_type
+M.handshake_type = {
+  client_hello = 1,
+  server_hello = 2,
+  new_session_ticket = 4,
+  end_of_early_data = 5,
+  encrypted_extensions = 8,
+  certificate = 11,
+  certificate_request = 13,
+  certificate_verify = 15,
+  finished = 20,
+  key_update = 24,
+  message_hash = 254,
+}
+
+---@enum http.cipher_suite
+M.cipher_suite = {
+  tls_aes_128_gcm_sha256 = string.char(0x13, 0x01),
+  tls_aes_256_gcm_sha384 = string.char(0x13, 0x02),
+  tls_chacha20_poly1305_sha256 = string.char(0x13, 0x03),
+  tls_aes_128_ccm_sha256 = string.char(0x13, 0x04),
+  tls_aes_128_ccm_8_sha256 = string.char(0x13, 0x05),
+}
+
+---@enum http.tls_version
+M.tls_version = {
+  _1_2 = string.char(0x03, 0x03),
+  _1_3 = string.char(0x03, 0x04),
+}
+
+local order = 0xff
+local irr_polynom = 0x11b
+local log = {} ---@type table<integer, integer>
+local exp = {} ---@type table<integer, integer>
+
+---@param a integer
+---@param b integer
+local function polynom_add(a, b) return bxor(a, b) end
+
+---@param a integer
+---@param b integer
+local function polynom_sub(a, b) return polynom_add(a, b) end
+
+--
+-- inverts element
+-- a^(-1) = g^(order - log(a))
+--
+---@param a integer
+---@return integer
+local function polynom_invert(a)
+  if a == 1 then return 1 end
+
+  local exponent = order - log[a]
+  return exp[exponent]
+end
+
+--
+-- multiply two elements using a logarithm table
+-- a*b = g^(log(a)+log(b))
+--
+---@param a integer
+---@param b integer
+---@return integer
+local function polynom_mul(a, b)
+  if a == 0 or b == 0 then return 0 end
+
+  local exponent = log[a] + log[b]
+  if exponent >= order then exponent = exponent - order end
+  return exp[exponent]
+end
+
+--
+-- divide two elements
+-- a/b = g^(log(a)-log(b))
+--
+---@param a integer
+---@param b integer
+---@return integer
+local function polynom_div(a, b)
+  if a == 0 then return 0 end
+  -- TODO: exception if operand2 == 0
+
+  local exponent = log[a] - log[b]
+  if exponent < 0 then exponent = exponent + order end
+  return exp[exponent]
+end
+
+local a = 1
+for i = 0, order - 1 do
+  exp[i] = a
+  log[a] = i
+
+  -- multiply with generator x+1 -> left shift + 1
+  a = bxor(bit.lshift(a, 1), a)
+
+  -- if a gets larger than order, reduce modulo irreducible polynom
+  if a > order then a = polynom_sub(a, irr_polynom) end
+end
+
+--
+-- calculate the parity of one byte
+--
+---@param byte integer
+---@return integer
+local function byte_parity(byte)
+  byte = bxor(byte, bit.rshift(byte, 4))
+  byte = bxor(byte, bit.rshift(byte, 2))
+  byte = bxor(byte, bit.rshift(byte, 1))
+  return bit.band(byte, 1)
+end
+
+---@param byte integer
+---@return integer
+local function affine_map(byte)
+  local mask = 0xf8
+  local result = 0
+  for i = 1, 8 do
+    result = bit.lshift(result, 1)
+
+    local parity = byte_parity(bit.band(byte, mask))
+    result = result + parity
+
+    -- simulate roll
+    local lastbit = bit.band(mask, 1)
+    mask = bit.band(bit.rshift(mask, 1), 0xff)
+    if lastbit ~= 0 then
+      mask = bit.bor(mask, 0x80)
+    else
+      mask = bit.band(mask, 0x7f)
+    end
+  end
+
+  return bxor(result, 0x63)
+end
+
+local s_box = {} ---@type table<integer, integer>
+local i_s_box = {} ---@type table<integer, integer>
+for i = 0, 255 do
+  local inverse ---@type integer
+  if i == 0 then
+    inverse = 0
+  else
+    inverse = polynom_invert(i)
+  end
+  local mapped = affine_map(inverse)
+  s_box[i] = mapped
+  i_s_box[mapped] = i
+end
+
+---@param a integer
+---@return integer
+local function xtime(a)
+  if bit.band(a, 0x80) ~= 0 then return bit.band(bxor(bit.lshift(a, 1), 0x1b), 0xff) end
+  return bit.lshift(a, 1)
+end
+
+-- rotate word: 0xaabbccdd gets 0xbbccddaa
+-- used for key schedule
+--
+---@param word integer[]
+---@return integer[]
+local function rot_word(word) return { word[2], word[3], word[4], word[1] } end
+
+-- replace all bytes in a word with the SBox.
+-- used for key schedule
+--
+---@param word integer[]
+---@return integer[]
+local function sub_word(word)
+  return iter(word):map(function(byte) return s_box[byte] end):totable()
+end
+
+local round_const = {
+  { 0x01, 0x00, 0x00, 0x00 },
+  { 0x02, 0x00, 0x00, 0x00 },
+  { 0x04, 0x00, 0x00, 0x00 },
+  { 0x08, 0x00, 0x00, 0x00 },
+  { 0x10, 0x00, 0x00, 0x00 },
+  { 0x20, 0x00, 0x00, 0x00 },
+  { 0x40, 0x00, 0x00, 0x00 },
+  { 0x80, 0x00, 0x00, 0x00 },
+  { 0x1b, 0x00, 0x00, 0x00 },
+  { 0x36, 0x00, 0x00, 0x00 },
+  { 0x6c, 0x00, 0x00, 0x00 },
+  { 0xd8, 0x00, 0x00, 0x00 },
+  { 0xab, 0x00, 0x00, 0x00 },
+  { 0x4d, 0x00, 0x00, 0x00 },
+  { 0x9a, 0x00, 0x00, 0x00 },
+  { 0x2f, 0x00, 0x00, 0x00 },
+}
+
+---@param state integer[][]
+local function inv_byte_sub(state)
+  for _, col in ipairs(state) do
+    for i, byte in ipairs(col) do
+      col[i] = i_s_box[byte]
+    end
+  end
+end
+
+---@param state integer[][]
+local function byte_sub(state)
+  for _, col in ipairs(state) do
+    for i, byte in ipairs(col) do
+      col[i] = s_box[byte]
+    end
+  end
+end
+
+---@param state integer[][]
+---@param n_columns integer
+local function mix_column(state, n_columns)
+  for i = 1, n_columns do
+    local bytes = state[i]
+
+    local t = bxor(bytes[1], bytes[2], bytes[3], bytes[4])
+    local u = bytes[1]
+    bytes[1] = bxor(bytes[1], t, xtime(bxor(bytes[1], bytes[2])))
+    bytes[2] = bxor(bytes[2], t, xtime(bxor(bytes[2], bytes[3])))
+    bytes[3] = bxor(bytes[3], t, xtime(bxor(bytes[3], bytes[4])))
+    bytes[4] = bxor(bytes[4], t, xtime(bxor(bytes[4], u)))
+  end
+end
+
+---@param state integer[][]
+---@param n_columns integer
+local function inv_mix_column(state, n_columns)
+  for i = 1, n_columns do
+    local bytes = state[i]
+
+    local u = xtime(xtime(bxor(bytes[1], bytes[3])))
+    local v = xtime(xtime(bxor(bytes[2], bytes[4])))
+    bytes[1] = bxor(bytes[1], u)
+    bytes[2] = bxor(bytes[2], v)
+    bytes[3] = bxor(bytes[3], u)
+    bytes[4] = bxor(bytes[4], v)
+  end
+  mix_column(state, n_columns)
+end
+
+---@param state integer[][]
+---@param n_columns integer
+---@return integer[][]
+local function inv_shift_row(state, n_columns)
+  local offset2 = n_columns == 4 and 1 or n_columns == 6 and 1 or n_columns == 8 and 1
+  local offset3 = n_columns == 4 and 2 or n_columns == 6 and 2 or n_columns == 8 and 3
+  local offset4 = n_columns == 4 and 3 or n_columns == 6 and 3 or n_columns == 8 and 4
+
+  local shifted = {} ---@type integer[][]
+  for i = 1, n_columns do
+    local col2 = i - offset2
+    if col2 <= 0 then col2 = col2 + n_columns end
+    local col3 = i - offset3
+    if col3 <= 0 then col3 = col3 + n_columns end
+    local col4 = i - offset4
+    if col4 <= 0 then col4 = col4 + n_columns end
+    shifted[i] = {
+      state[i][1],
+      state[col2][2],
+      state[col3][3],
+      state[col4][4],
+    }
+  end
+  return shifted
+end
+
+---@param state integer[][]
+---@param n_columns integer
+---@return integer[][]
+local function shift_row(state, n_columns)
+  local offset2 = n_columns == 4 and 1 or n_columns == 6 and 1 or n_columns == 8 and 1
+  local offset3 = n_columns == 4 and 2 or n_columns == 6 and 2 or n_columns == 8 and 3
+  local offset4 = n_columns == 4 and 3 or n_columns == 6 and 3 or n_columns == 8 and 4
+
+  local shifted = {} ---@type integer[][]
+  for i = 1, n_columns do
+    local col2 = i + offset2
+    if col2 > n_columns then col2 = col2 - n_columns end
+    local col3 = i + offset3
+    if col3 > n_columns then col3 = col3 - n_columns end
+    local col4 = i + offset4
+    if col4 > n_columns then col4 = col4 - n_columns end
+    shifted[i] = {
+      state[i][1],
+      state[col2][2],
+      state[col3][3],
+      state[col4][4],
+    }
+  end
+  return shifted
+end
+
+---@param state integer[][]
+---@param key_schedule integer[][]
+---@return integer[][]
+local function add_round_key(state, n_columns_key, round, key_schedule)
+  return iter(ipairs(state))
+    :map(function(i, col)
+      return iter(ipairs(col))
+        :map(function(j, byte)
+          local round_key_col = key_schedule[i + n_columns_key * round]
+          local round_key_byte = round_key_col[j]
+          return bxor(byte, round_key_byte)
+        end)
+        :totable()
+    end)
+    :totable()
+end
+
+---@param cipher_key integer[][]
+---@param n_columns integer
+---@param n_columns_key integer
+---@param n_rounds integer
+---@return integer[][]
+local function expand_cipher_key(cipher_key, n_columns, n_columns_key, n_rounds)
+  local key_schedule = {} ---@type integer[][]
+  if n_columns_key < 6 then
+    for i, col in ipairs(cipher_key) do
+      key_schedule[i] = col
+    end
+    for i = n_columns_key + 1, n_columns * (n_rounds + 1) do
+      local temp = key_schedule[i - 1]
+      if ((i - 1) % n_columns_key) == 0 then
+        temp = iter(ipairs(sub_word(rot_word(temp))))
+          :map(function(j, byte) return bxor(byte, round_const[(i - 1) / n_columns_key][j]) end)
+          :totable()
+      end
+      key_schedule[i] = iter(ipairs(key_schedule[i - n_columns_key]))
+        :map(function(j, byte) return bxor(byte, temp[j]) end)
+        :totable()
+    end
+  else
+    for i, col in ipairs(cipher_key) do
+      key_schedule[i] = col
+    end
+    for i = n_columns_key + 1, n_columns * (n_rounds + 1) do
+      local temp = key_schedule[i - 1]
+      if ((i - 1) % n_columns_key) == 0 then
+        temp = iter(ipairs(sub_word(rot_word(temp))))
+          :map(function(j, byte) return bxor(byte, round_const[(i - 1) / n_columns_key][j]) end)
+          :totable()
+      elseif ((i - 1) % n_columns_key) == 4 then
+        temp = sub_word(temp) -- this is the only different line
+      end
+      key_schedule[i] = iter(ipairs(key_schedule[i - n_columns_key]))
+        :map(function(j, byte) return bxor(byte, temp[j]) end)
+        :totable()
+    end
+  end
+  return key_schedule
+end
+
+---@param block_length integer 128|192|256
+---@param key_length integer 128|192|256
+---@param key string
+---@param plaintext string
+local function aes_encrypt(block_length, key_length, key, plaintext)
+  local n_columns = block_length / 32 -- Nb
+  local n_columns_key = key_length / 32 -- Nk
+
+  -- Nr
+  local n_rounds = (n_columns == 8 or n_columns_key == 8) and 14
+    or (n_columns == 6 or n_columns_key == 6) and 12
+    or (n_columns == 4 and n_columns_key == 4) and 10
+    or nil
+  assert(n_rounds)
+
+  local state = {} ---@type integer[][]
+  for i = 1, n_columns do
+    local byte1 = plaintext:sub(i * 4 - 3, i * 4 - 3):byte()
+    local byte2 = plaintext:sub(i * 4 - 2, i * 4 - 2):byte()
+    local byte3 = plaintext:sub(i * 4 - 1, i * 4 - 1):byte()
+    local byte4 = plaintext:sub(i * 4, i * 4):byte()
+
+    table.insert(state, { byte1, byte2, byte3, byte4 })
+  end
+
+  local cipher_key = {} ---@type integer[][]
+  for i = 1, n_columns_key do
+    local byte1 = key:sub(i * 4 - 3, i * 4 - 3):byte()
+    local byte2 = key:sub(i * 4 - 2, i * 4 - 2):byte()
+    local byte3 = key:sub(i * 4 - 1, i * 4 - 1):byte()
+    local byte4 = key:sub(i * 4, i * 4):byte()
+
+    table.insert(cipher_key, { byte1, byte2, byte3, byte4 })
+  end
+
+  ---@type integer[][]
+  state = iter(ipairs(state))
+    :map(function(i, col)
+      return iter(ipairs(col)):map(function(j, byte) return bxor(byte, cipher_key[i][j]) end):totable()
+    end)
+    :totable()
+
+  local key_schedule = expand_cipher_key(cipher_key, n_columns, n_columns_key, n_rounds)
+
+  for round = 1, n_rounds - 1 do
+    byte_sub(state)
+
+    state = shift_row(state, n_columns)
+
+    mix_column(state, n_columns)
+
+    state = add_round_key(state, n_columns_key, round, key_schedule)
+  end
+
+  byte_sub(state)
+
+  state = shift_row(state, n_columns)
+
+  local round = n_rounds
+  state = add_round_key(state, n_columns_key, round, key_schedule)
+  return state
+
+  -- TODO: what is the correct interface for both if this functions? return integer[][]?
+  -- local out = {} ---@type string[]
+  -- for i = 1, n_columns do
+  --   for _, byte in ipairs(state[i]) do
+  --     table.insert(out, bit.tohex(byte, 2))
+  --   end
+  -- end
+  -- return out
+end
+
+---@param block_length integer 128|192|256
+---@param key_length integer 128|192|256
+---@param key string
+---@param ciphered_text integer[][]
+local function aes_decrypt(block_length, key_length, key, ciphered_text)
+  local n_columns = block_length / 32 -- Nb
+  local n_columns_key = key_length / 32 -- Nk
+
+  -- Nr
+  local n_rounds = (n_columns == 8 or n_columns_key == 8) and 14
+    or (n_columns == 6 or n_columns_key == 6) and 12
+    or (n_columns == 4 and n_columns_key == 4) and 10
+    or nil
+  assert(n_rounds)
+
+  local state = ciphered_text
+
+  local cipher_key = {} ---@type integer[][]
+  for i = 1, n_columns_key do
+    local byte1 = key:sub(i * 4 - 3, i * 4 - 3):byte()
+    local byte2 = key:sub(i * 4 - 2, i * 4 - 2):byte()
+    local byte3 = key:sub(i * 4 - 1, i * 4 - 1):byte()
+    local byte4 = key:sub(i * 4, i * 4):byte()
+
+    table.insert(cipher_key, { byte1, byte2, byte3, byte4 })
+  end
+
+  local key_schedule = expand_cipher_key(cipher_key, n_columns, n_columns_key, n_rounds)
+
+  local round = n_rounds
+  state = add_round_key(state, n_columns_key, round, key_schedule)
+
+  state = inv_shift_row(state, n_columns)
+
+  inv_byte_sub(state)
+
+  for round = n_rounds - 1, 1, -1 do
+    state = add_round_key(state, n_columns_key, round, key_schedule)
+
+    inv_mix_column(state, n_columns)
+
+    state = inv_shift_row(state, n_columns)
+
+    inv_byte_sub(state)
+  end
+
+  ---@type integer[][]
+  state = iter(ipairs(state))
+    :map(function(i, col)
+      return iter(ipairs(col)):map(function(j, byte) return bxor(byte, cipher_key[i][j]) end):totable()
+    end)
+    :totable()
+
+  local out = {} ---@type string[]
+  for i = 1, n_columns do
+    for _, byte in ipairs(state[i]) do
+      table.insert(out, bit.tohex(byte, 2))
+    end
+  end
+  return out
+end
+
+local encrypted = aes_encrypt(128, 128, "1234567890123456", "algo            ")
+local decrypted = aes_decrypt(128, 128, "1234567890123456", encrypted)
+-- __AUTO_GENERATED_PRINT_VAR_START__
+print([==[ decrypted:]==], vim.inspect(decrypted)) -- __AUTO_GENERATED_PRINT_VAR_END__
+
+---@param p string plaintext
+---@param aad string aditional authenticated data
+---@param iv string initialization vector (a nonce)
+---@return string c ciphertext
+---@return string t authentication tag, its length must be 128, 120, 112, 104, or 96
+local function gcm_encrypt(p, aad, iv) end
+
+---@param aad string
+---@param iv string
+---@param c string
+---@param t string
+---@return string p
+local function gcm_decrypt(aad, iv, c, t) end
+
+---@param str string
+local function to_hex(str)
+  local out = {} ---@type string[]
+  for i = 1, #str do
+    local byte = str:sub(i, i):byte()
+    table.insert(out, bit.tohex(byte, 2))
+  end
+  return table.concat(out)
+end
+
+-- key in hex: 31323334353637383930313233343536
+-- text in hex: 616c676f202020202020202020202020
+-- encrypted in hex: 1a1f74115e439103990da1b5e66bb215
+
+-- block chiper: AES-128
+---@param p string plaintext
+---@param aad string aditional authenticated data
+---@param iv string initialization vector (a nonce)
+---@return string c ciphertext
+---@return string t authentication tag
+local function aes_128_gcm_sha256(p, aad, iv) end
+
+local function tls(uri)
+  -- TODO: handle http vs https
+  local co = coroutine.running()
+  assert(co, "get must be called within a coroutine")
+
+  local parsed_uri = assert(uri_grammar:match(uri)) ---@type http.Uri
+  local host = assert(parsed_uri.hier_part.authority.host)
+  local port = parsed_uri.port or port_by_scheme[parsed_uri.scheme]
+  local path = assert(parsed_uri.hier_part.path)
+
+  local resolved_host = uv.getaddrinfo(host, nil, {
+    family = "inet",
+    protocol = "tcp",
+  })[1]
+
+  assert(resolved_host, ("Host `%s`, can't be resolved to a valid IP address"):format(host))
+
+  local client = assert(uv.new_tcp())
+  client:connect(resolved_host.addr, port, function(err)
+    assert(not err, err)
+    co_resume(co)
+  end)
+  coroutine.yield()
+
+  client:read_start(function(err, data)
+    assert(not err, err)
+    if not data then return end
+
+    co_resume(co, data) -- will resume the last coroutine.yield, before returning from `get`
+  end)
+
+  local legacy_version = M.tls_version._1_2
+  local random_32_bytes = random()
+  local legacy_session_id = "\0"
+  local cipher_suite = table.concat {
+    M.cipher_suite.tls_aes_128_gcm_sha256,
+    M.cipher_suite.tls_aes_256_gcm_sha384,
+    M.cipher_suite.tls_chacha20_poly1305_sha256,
+    M.cipher_suite.tls_aes_128_ccm_sha256,
+    M.cipher_suite.tls_aes_128_ccm_8_sha256,
+  }
+  local legacy_compression_methods = "\0"
+  -- tls extensions
+  local supported_versions = M.tls_version._1_3
+  local request = (string.rep("%s", 6)):format(
+    legacy_version,
+    random_32_bytes,
+    legacy_session_id,
+    cipher_suite,
+    legacy_compression_methods,
+    supported_versions
+  )
+  client:write(request, function(err)
+    assert(not err, err)
+
+    co_resume(co)
+  end)
+  coroutine.yield(co)
+
+  local response = coroutine.yield() ---@type string
+  client:read_stop()
+  client:close()
+  return http_grammar:match(response)
+end
+
+-- tls "https://httbingo.org/get"
+
+return M
