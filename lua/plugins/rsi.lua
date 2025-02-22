b09c11d884cbe7c7045f284e89378bd54360cf79
TheLeoP <eugenio2305@hotmail.com>
2025-02-22 11:29:54 -0500

    (rsi) only use command mode keymaps

--- lua/plugins/rsi.lua
+++ lua/plugins/rsi.lua
@@ -1,3 +1,20 @@
+local keymap = vim.keymap
+
 return {
   "tpope/vim-rsi",
+  config = function()
+    -- Only use command mode keymaps
+    keymap.del("i", "<C-a>")
+    keymap.del("i", "<C-x><C-a>")
+    keymap.del("i", "<C-b>")
+    keymap.del("i", "<C-d>")
+    keymap.del("i", "<C-e>")
+    keymap.del("i", "<C-f>")
+    keymap.del("i", "<M-BS>")
+    keymap.del("i", "<M-b>")
+    keymap.del("i", "<M-d>")
+    keymap.del("i", "<M-f>")
+    keymap.del("i", "<M-n>")
+    keymap.del("i", "<M-p>")
+  end,
 }
