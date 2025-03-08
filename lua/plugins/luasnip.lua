9e2a2245b395a1345da298c639e125e5743f9090
TheLeoP <eugenio2305@hotmail.com>
2025-03-07 21:29:29 -0500

    (luasnip) navigate placeholders with left and right

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -16,8 +16,8 @@ return {
     local ls = require "luasnip"
     ls.setup(opts)
     keymap.set({ "i" }, "<C-j>", function() ls.expand() end)
-    keymap.set({ "i", "s" }, "<down>", function() ls.jump(1) end)
-    keymap.set({ "i", "s" }, "<up>", function() ls.jump(-1) end)
+    keymap.set({ "i", "s" }, "<right>", function() ls.jump(1) end)
+    keymap.set({ "i", "s" }, "<left>", function() ls.jump(-1) end)
 
     keymap.set("i", "<a-b>", function()
       if ls.choice_active() then require "luasnip.extras.select_choice"() end
