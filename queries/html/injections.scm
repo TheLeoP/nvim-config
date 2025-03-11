b3afa95bb7d384cfc7c5919570376959cb71e485
TheLeoP <eugenio2305@hotmail.com>
2025-03-11 16:08:37 -0500

    (html) add css injections

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -5,3 +5,7 @@
     (attribute_value) @injection.content)
   (#eq? @name "onClick")
   (#set! injection.language "javascript"))
+
+(style_element
+  (raw_text) @injection.content
+  (#set! injection.language "css"))
