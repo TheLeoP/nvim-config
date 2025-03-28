d60f3af92ba96bbcc4b9b0e57eaafe559ad442ea
TheLeoP <eugenio2305@hotmail.com>
2025-03-28 12:44:18 -0500

    (js) make auto-async an expr keymap to work better with macros

--- lua/personal/util/js.lua
+++ lua/personal/util/js.lua
@@ -23,44 +23,46 @@ function M.setup()
   }
 
   vim.keymap.set("i", "t", function()
-    api.nvim_feedkeys("t", "n", false)
+    vim.schedule(function()
+      local row, col = unpack(api.nvim_win_get_cursor(0))
+      local start_col = math.max(col - 5, 0)
+      local end_col = col
+      local word = api.nvim_buf_get_text(0, row - 1, start_col, row - 1, end_col, {})[1]
+      if word ~= "await" then return end
 
-    local row, col = unpack(api.nvim_win_get_cursor(0))
-    local start_col = math.max(col - 4, 0)
-    local end_col = col
-    local word = api.nvim_buf_get_text(0, row - 1, start_col, row - 1, end_col, {})[1]
-    -- `awai` because this gets called before the `t` is added to the buffer
-    if word ~= "awai" then return end
+      ts.get_parser(0):parse()
+      local node = ts.get_node { ignore_injections = false }
+      if not node then return end
 
-    ts.get_parser(0):parse()
-    local node = ts.get_node { ignore_injections = false }
-    if not node then return end
-
-    local current = node ---@type TSNode|nil
-    while current do
-      if
-        vim.list_contains({
-          "arrow_function",
-          "function_declaration",
-          "function",
-          "method_definition",
-          "function_expression",
-        }, current:type())
-      then
-        break
+      local current = node ---@type TSNode|nil
+      while current do
+        if
+          vim.list_contains({
+            "arrow_function",
+            "function_declaration",
+            "function",
+            "method_definition",
+            "function_expression",
+          }, current:type())
+        then
+          break
+        end
+        current = current:parent()
       end
-      current = current:parent()
-    end
-    if not current then return end
-    local function_node = current ---@type TSNode
+      if not current then return end
+      local function_node = current ---@type TSNode
 
-    local function_text = ts.get_node_text(function_node, 0)
-    if vim.startswith(function_text, "async") then return end
+      local function_text = ts.get_node_text(function_node, 0)
+      if vim.startswith(function_text, "async") then return end
 
-    local start_row, start_col = function_node:start()
-    vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { "async " })
+      local start_row, start_col = function_node:start()
+      vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { "async " })
+    end)
+
+    return "t"
   end, {
     buffer = true,
+    expr = true,
   })
 end
 
