1d0c87a537aed126ac5541aeea455c4c93037af0
TheLeoP <eugenio2305@hotmail.com>
2025-03-29 20:06:28 -0500

    feat(overseer): change toggle keymap, don't open on rerun, keymap for opening in tab

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -13,18 +13,10 @@ local function restart_last_task()
   local tasks = overseer.list_tasks { recent_first = true }
   if vim.tbl_isempty(tasks) then
     vim.notify("No tasks found", vim.log.levels.WARN)
-  else
-    overseer.run_action(tasks[1], "restart")
-    open_and_close()
+    return
   end
-end
-
-local function run_task()
-  local overseer = require "overseer"
-  overseer.run_template({}, function(task)
-    if not task then return end
-    open_and_close()
-  end)
+  overseer.run_action(tasks[1], "restart")
+  vim.notify "Running last task"
 end
 
 local function start_prompt()
@@ -62,6 +54,7 @@ return {
         direction = "bottom",
         max_width = { 600, 0.7 },
         bindings = {
+          ["<C-t>"] = "<CMD>OverseerQuickAction open tab<CR>",
           ["<C-b>"] = "ScrollOutputUp",
           ["<C-f>"] = "ScrollOutputDown",
           ["H"] = "IncreaseAllDetail",
@@ -88,9 +81,16 @@ return {
       local overseer = require "overseer"
       overseer.setup(opts)
 
-      vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<cr>", { desc = "Toggle task window" })
+      -- follows my general `toggle` convention for keymaps
+      vim.keymap.set("n", "<leader>to", "<cmd>OverseerToggle<cr>", { desc = "Toggle task window" })
+
       vim.keymap.set("n", "<leader>o<", restart_last_task, { desc = "Restart last task" })
-      vim.keymap.set("n", "<leader>or", run_task, { desc = "Run task" })
+      vim.keymap.set("n", "<leader>or", function()
+        overseer.run_template({}, function(task)
+          if not task then return end
+          open_and_close()
+        end)
+      end, { desc = "Run task" })
 
       -- vim-dispatch style keymaps
       vim.keymap.set("n", "' ", start_prompt)
