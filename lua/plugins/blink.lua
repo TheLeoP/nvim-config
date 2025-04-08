7c520bb3a7fcf74776dad5377435de1cdfaabebb
TheLeoP <eugenio2305@hotmail.com>
2025-04-08 15:22:09 -0500

    (blink) use all non special buffers to get completions from

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -127,6 +127,14 @@ return {
             get_cwd = function(_) return vim.fn.getcwd() end,
           },
         },
+
+        buffer = {
+          opts = {
+            get_bufnrs = function()
+              return vim.tbl_filter(function(bufnr) return vim.bo[bufnr].buftype == "" end, vim.api.nvim_list_bufs())
+            end,
+          },
+        },
       },
     },
   },
