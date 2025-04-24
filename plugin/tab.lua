8a478413c0d4b5e967892f8cea910c506b33a5d7
TheLeoP <eugenio2305@hotmail.com>
2025-04-24 08:10:36 -0500

    (tabline) use a custom tabline to only show file name in tabline

---
+++ plugin/tab.lua
@@ -0,0 +1,30 @@
+local api = vim.api
+
+function _G._personal_tab()
+  local last = vim.fn.tabpagenr "$"
+  local current = vim.fn.tabpagenr()
+
+  local out = {} ---@type string[]
+  for i = 1, last do
+    table.insert(out, i == current and "%#TabLineSel#" or "%#TabLine#")
+    table.insert(out, ("%%%dT"):format(i)) -- set the tab page number (for mouse clicks)
+
+    table.insert(out, (" %%{v:lua._personal_tab_label(%d)} "):format(i))
+  end
+  table.insert(out, "%#TabLineFill#%T")
+  table.insert(out, "%=%#TabLine#%999XX")
+
+  return table.concat(out)
+end
+
+---@param i integer
+function _G._personal_tab_label(i)
+  local buflist = vim.fn.tabpagebuflist(i) ---@type integer[]
+  local winnr = vim.fn.tabpagewinnr(i)
+  local buf = buflist[winnr]
+  local name = api.nvim_buf_get_name(buf)
+  local tail = vim.fn.fnamemodify(name, ":t")
+  return tail
+end
+
+vim.o.tabline = "%{%v:lua._personal_tab()%}"
