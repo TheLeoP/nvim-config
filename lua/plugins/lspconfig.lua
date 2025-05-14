dc179fce00284ff7377a3becf391d222624f5b7f
TheLeoP <eugenio2305@hotmail.com>
2025-05-14 12:57:33 -0500

    (lspconfig) enable all language server in a single vim.lsp.enable call

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,25 +1,27 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.lsp.enable "basedpyright"
-    vim.lsp.enable "angularls"
-    vim.lsp.enable "fennel_language_server"
-    vim.lsp.enable "vimls"
-    vim.lsp.enable "buf_ls"
-    vim.lsp.enable "html"
-    vim.lsp.enable "cssls"
-    vim.lsp.enable "lemminx"
-    vim.lsp.enable "phpactor"
-    vim.lsp.enable "prismals"
-    vim.lsp.enable "marksman"
-    vim.lsp.enable "dockerls"
-    vim.lsp.enable "docker_compose_language_service"
-    vim.lsp.enable "lua_ls"
-    vim.lsp.enable "gopls"
-    vim.lsp.enable "jsonls"
-    vim.lsp.enable "groovyls"
-    vim.lsp.enable "emmet_language_server"
-    vim.lsp.enable "tailwindcss"
-    vim.lsp.enable "clangd"
+    vim.lsp.enable {
+      "basedpyright",
+      "angularls",
+      "fennel_language_server",
+      "vimls",
+      "buf_ls",
+      "html",
+      "cssls",
+      "lemminx",
+      "phpactor",
+      "prismals",
+      "marksman",
+      "dockerls",
+      "docker_compose_language_service",
+      "lua_ls",
+      "gopls",
+      "jsonls",
+      "groovyls",
+      "emmet_language_server",
+      "tailwindcss",
+      "clangd",
+    }
   end,
 }
