f99a138b58cde9062207060a3742d0dba35a8be7
TheLeoP <eugenio2305@hotmail.com>
2025-06-02 11:17:37 -0500

    (matchup) enable treesitter on my fork

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -1,9 +1,12 @@
 return {
-  "andymass/vim-matchup",
+  "theleop/vim-matchup",
+  branch = "update-treesitter",
   init = function()
     vim.g.matchup_delim_noskips = 1 -- recognize only symbols in strings and comments
     vim.g.matchup_matchparen_offscreen = {} -- disable feature
     vim.g.matchup_matchparen_deferred = 1
     vim.g.matchup_mouse_enabled = 0
+
+    vim.g.matchup_treesitter_enabled = true
   end,
 }
