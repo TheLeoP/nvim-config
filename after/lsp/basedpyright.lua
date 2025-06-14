750ef1a3ac6e53bcbb1cf67e0360e41986209ee6
TheLeoP <eugenio2305@hotmail.com>
2025-06-14 10:34:21 -0500

    (basedpyright) remove custom root_dir logic for monorepos

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -1,18 +1,4 @@
 return {
-  ---@param buf integer
-  ---@param cb fun(root_dir: string)
-  root_dir = function(buf, cb)
-    local root = vim.fs.root(buf, {
-      "setup.cfg",
-      "pyproject.toml",
-      "setup.cfg",
-      "requirements.txt",
-      "Pipfile",
-      "pyrightconfig.json",
-    })
-    if not root then return end
-    cb(root)
-  end,
   settings = {
     basedpyright = {
       analysis = {
