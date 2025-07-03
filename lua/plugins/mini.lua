5563fece45e723e2425ca83ff23f534e84c31ef5
TheLeoP <eugenio2305@hotmail.com>
2025-07-03 10:31:29 -0500

    fix(ts-context-commentstring): override c default commentstring to follow defaults

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -79,8 +79,13 @@ return {
     "nvim-treesitter-textobjects",
     {
       "JoosepAlviste/nvim-ts-context-commentstring",
+      ---@module "ts_context_commentstring"
+      ---@type ts_context_commentstring.Config
       opts = {
         enable_autocmd = false,
+        languages = {
+          c = "// %s",
+        },
       },
     },
   },
