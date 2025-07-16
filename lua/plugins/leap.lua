a89c935e1003f6e53d5b8a586b7afc33807914c7
TheLeoP <eugenio2305@hotmail.com>
2025-07-16 09:14:56 -0500

    feat(leap)!: adjust to new defaults, use ftFT motions for fine grain control and create jumplist entries before each jump

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap
+local api = vim.api
 
 return {
   {
@@ -8,24 +9,42 @@ return {
       local leap = require "leap"
       leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
 
-      keymap.set({ "n" }, "s", function()
-        leap.leap {}
-      end, { desc = "leap" })
-      keymap.set({ "x", "o" }, "s", function()
+      keymap.set({ "n", "x", "o" }, "st", function()
         leap.leap { offset = -1, inclusive_op = true }
       end, { desc = "leap until" })
-      keymap.set({ "n" }, "S", function()
-        leap.leap { backward = true }
-      end, { desc = "leap backward" })
-      keymap.set({ "x", "o" }, "S", function()
-        local mode = vim.api.nvim_get_mode().mode
-        if mode ~= "v" and mode ~= "V" and mode ~= "\22" then
-          -- simulates backward inclusive_op because it's broken on leap.nvim
-          vim.cmd.normal "v"
-        end
-        leap.leap { backward = true, offset = 2 }
-      end, { desc = "leap until backard" })
-      keymap.set({ "n", "x", "o" }, "gs", function()
+      keymap.set({ "n", "x", "o" }, "sf", function()
+        leap.leap { inclusive_op = true }
+      end, { desc = "leap find" })
+
+      keymap.set({ "n", "x", "o" }, "sT", function()
+        leap.leap { backward = true, offset = 1, backward_inclusive = true }
+      end, { desc = "leap until backward" })
+      keymap.set({ "n", "x", "o" }, "sF", function()
+        leap.leap { backward = true, backward_inclusive = true }
+      end, { desc = "leap find backward" })
+      api.nvim_create_augroup("personal-leap", { clear = true })
+      api.nvim_create_autocmd("User", {
+        pattern = "LeapEnter",
+        group = "personal-leap",
+        callback = function()
+          local mode = vim.api.nvim_get_mode().mode
+          if (mode ~= "v" and mode ~= "V" and mode ~= "\22" and mode ~= "n") and leap.state.args.backward_inclusive then
+            -- NOTE: force characterwise visual mode to make backwards
+            -- operations inclusive
+            vim.cmd.normal "v"
+          end
+        end,
+      })
+      api.nvim_create_autocmd("User", {
+        pattern = "LeapEnter",
+        group = "personal-leap",
+        callback = function()
+          -- NOTE: create a jumplist entry before each jump
+          vim.cmd.normal "m'"
+        end,
+      })
+
+      keymap.set({ "n", "x", "o" }, "sn", function()
         require("leap.treesitter").select()
       end, { desc = "leap treesitter" })
 
