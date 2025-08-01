cb263c8101ff104a2536922b86174188339d5a41
TheLeoP <eugenio2305@hotmail.com>
2025-08-01 18:02:22 -0500

    fix: highlight text on yank again

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,5 +1,14 @@
 local uv = vim.uv
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
+  desc = "Highlights yanked area",
+  callback = function()
+    -- Setting a priority higher than the LSP references one.
+    vim.highlight.on_yank { higroup = "Visual", priority = 4200 }
+  end,
+})
+
 vim.api.nvim_create_autocmd("CmdwinEnter", {
   group = vim.api.nvim_create_augroup("ExecuteCmdAndStay", { clear = true }),
   desc = "Execute command and stay in the command-line window",
