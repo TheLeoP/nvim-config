84d9c19528a6b39468c1ac5467d182d0ac8caaa7
TheLeoP <eugenio2305@hotmail.com>
2025-08-14 10:50:44 -0500

    feat(illuminate): disable in most non-normal modes, increase min_count

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -2,12 +2,34 @@ return {
   "RRethy/vim-illuminate",
   config = function()
     require("illuminate").configure {
-      filetype_overrides = {
-        cs = { providers = { "treesitter", "regex" } },
+      modes_denylist = {
+        "i",
+        "ic",
+        "ix",
+
+        "v",
+        "V",
+        "\22",
+
+        "R",
+        "Rc",
+        "Rx",
+        "Rv",
+        "Rvc",
+        "Rvx",
+
+        "c",
+        "cv",
+
+        "r",
+        "rm",
+        "r?",
+        "t",
       },
       filetypes_denylist = {
         "",
       },
+      min_count_to_highlight = 2,
     }
   end,
 }
