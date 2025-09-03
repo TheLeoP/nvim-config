abfd91ce0051824150bdcab5a275141567701fec
TheLeoP <eugenio2305@hotmail.com>
2025-09-03 11:31:15 -0500

    feat(ts-context): add powershell captures for ts-context

---
+++ queries/powershell/context.scm
@@ -0,0 +1,25 @@
+(function_statement) @context
+
+(if_statement) @context
+
+(else_clause) @context
+
+(elseif_clause) @context
+
+(switch_statement) @context
+
+(switch_clause) @context
+
+(try_statement) @context
+
+(foreach_statement) @context
+
+(for_statement) @context
+
+(while_statement) @context
+
+(class_statement) @context
+
+(class_method_definition) @context
+
+(command) @context
