2b8b1986c8ad5f84990e410500645b1a7f7fbf34
TheLeoP <eugenio2305@hotmail.com>
2025-09-23 09:41:50 -0500

    feat(powershell): create toggle keymap inside terminal buffer

--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,27 +1,38 @@
----@module "powershell"
 return {
   "TheLeoP/powershell.nvim",
   dev = true,
-  ---@type powershell.user_config
-  opts = {
-    capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
-    bundle_path = vim.fs.normalize(require("personal.config.lsp").mason_root .. "powershell-editor-services"),
-    init_options = {
-      enableProfileLoading = false,
-    },
-    settings = {
-      powershell = {
-        codeFormatting = {
-          openBraceOnSameLine = true,
+  config = function()
+    require("powershell").setup {
+      capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
+      bundle_path = vim.fs.normalize(require("personal.config.lsp").mason_root .. "powershell-editor-services"),
+      init_options = {
+        enableProfileLoading = false,
+      },
+      settings = {
+        powershell = {
+          codeFormatting = {
+            openBraceOnSameLine = true,
 
-          useConstantStrings = true,
-          useCorrectCasing = true,
-          whitespaceAroundOperator = true,
-          whitespaceAfterSeparator = true,
-          whitespaceBeforeOpenBrace = true,
-          addWhitespaceAroundPipe = true,
+            useConstantStrings = true,
+            useCorrectCasing = true,
+            whitespaceAroundOperator = true,
+            whitespaceAfterSeparator = true,
+            whitespaceBeforeOpenBrace = true,
+            addWhitespaceAroundPipe = true,
+          },
         },
       },
-    },
-  },
+    }
+
+    local augroup = vim.api.nvim_create_augroup("personal-powershell", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = augroup,
+      pattern = "powershell.nvim-term",
+      callback = function(opts)
+        vim.keymap.set("n", "<leader>P", function()
+          require("powershell").toggle_term()
+        end, { buffer = opts.data.buf })
+      end,
+    })
+  end,
 }
