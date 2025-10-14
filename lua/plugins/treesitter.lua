32d9831f9f9d9285987c0ff2ea1b09edca656e28
TheLeoP <eugenio2305@hotmail.com>
2025-10-14 12:58:23 -0500

    refactor: simplify treesitter language installation and highlight start
    nvim-treesitter registers the angular parser for the `htmlangular`
    filetype, which is detected by Neovim without issues.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,103 +1,95 @@
 local api = vim.api
 local iter = vim.iter
+local ts = vim.treesitter
 
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   build = ":TSUpdate",
   config = function()
-    ---@type {parser: string, ft: string[]?}[]
-    local ts_info = {
-      { parser = "bash", ft = { "bash" } },
-      { parser = "c", ft = { "c" } },
-      { parser = "lua", ft = { "lua" } },
-      { parser = "vim", ft = { "vim" } },
-      { parser = "xml", ft = { "xml" } },
-      { parser = "http", ft = { "http" } },
-      { parser = "json", ft = { "json" } },
-      { parser = "graphql", ft = { "graphql" } },
-      { parser = "query", ft = { "query" } },
-      { parser = "vimdoc", ft = { "help" } },
-      { parser = "c_sharp", ft = { "cs" } },
-      { parser = "cpp", ft = { "cpp" } },
-      { parser = "css", ft = { "css" } },
-      { parser = "dockerfile", ft = { "dockerfile" } },
-      { parser = "editorconfig", ft = { "editorconfig" } },
-      { parser = "git_config", ft = { "gitconfig" } },
-      { parser = "git_rebase", ft = { "gitrebase" } },
-      { parser = "gitattributes", ft = { "gitattributes" } },
-      { parser = "gitcommit", ft = { "gitcommit" } },
-      { parser = "gitignore", ft = { "gitignore" } },
-      { parser = "go", ft = { "go" } },
-      { parser = "gomod", ft = { "gomod" } },
-      { parser = "gosum", ft = { "gosum" } },
-      { parser = "groovy", ft = { "groovy" } },
-      { parser = "html", ft = { "html" } },
-      { parser = "javascript", ft = { "javascript", "javascriptreact" } },
-      { parser = "typescript", ft = { "typescript" } },
-      { parser = "tsx", ft = { "typescriptreact" } },
-      { parser = "java", ft = { "java" } },
-      { parser = "ini", ft = { "ini" } },
-      { parser = "jsonc", ft = { "jsonc" } },
-      { parser = "make", ft = { "make" } },
-      { parser = "markdown", ft = { "markdown" } },
-      { parser = "pem", ft = { "pem" } },
-      { parser = "php", ft = { "php" } },
-      { parser = "proto", ft = { "proto" } },
-      { parser = "python", ft = { "python" } },
-      { parser = "ruby", ft = { "ruby" } },
-      { parser = "sql", ft = { "sql" } },
-      { parser = "ssh_config", ft = { "sshconfig" } },
-      { parser = "toml", ft = { "toml" } },
-      { parser = "vue", ft = { "vue" } },
-      { parser = "yaml", ft = { "yaml" } },
-      { parser = "diff", ft = { "diff" } },
-      { parser = "powershell", ft = { "ps1" } },
-      { parser = "astro", ft = { "astro" } },
-      { parser = "desktop", ft = { "desktop" } },
-      { parser = "blade", ft = { "blade" } },
+    ---@type string[]
+    local ensure_langs = {
+      "bash",
+      "c",
+      "lua",
+      "vim",
+      "xml",
+      "http",
+      "json",
+      "graphql",
+      "query",
+      "vimdoc",
+      "c_sharp",
+      "cpp",
+      "css",
+      "dockerfile",
+      "editorconfig",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "gomod",
+      "gosum",
+      "groovy",
+      "html",
+      "javascript",
+      "typescript",
+      "tsx",
+      "java",
+      "ini",
+      "jsonc",
+      "make",
+      "markdown",
+      "pem",
+      "php",
+      "proto",
+      "python",
+      "ruby",
+      "sql",
+      "ssh_config",
+      "toml",
+      "vue",
+      "yaml",
+      "diff",
+      "powershell",
+      "astro",
+      "desktop",
+      "blade",
 
-      { parser = "doxygen" },
-      { parser = "re2c" },
-      { parser = "luap" },
-      { parser = "printf" },
-      { parser = "luadoc" },
-      { parser = "jsdoc" },
-      { parser = "regex" },
-      { parser = "angular" },
-      { parser = "scss" },
-      { parser = "phpdoc" },
+      "doxygen",
+      "re2c",
+      "luap",
+      "printf",
+      "luadoc",
+      "jsdoc",
+      "regex",
+      "angular",
+      "scss",
+      "phpdoc",
     }
-    local ensure_installed = iter(ts_info)
-      :map(function(info)
-        return info.parser
-      end)
-      :totable()
     local already_installed = require("nvim-treesitter.config").get_installed "parsers"
-    local to_install = iter(ensure_installed)
+    local to_install = iter(ensure_langs)
       :filter(function(p)
         return not vim.tbl_contains(already_installed, p)
       end)
       :totable()
     if #to_install > 0 then require("nvim-treesitter").install(to_install) end
 
-    local pattern = iter(ts_info)
-      :map(function(info)
-        return info.ft
-      end)
+    local filetypes = iter(ensure_langs)
+      :map(
+        ---@param lang string
+        function(lang)
+          return ts.language.get_filetypes(lang)
+        end
+      )
       :flatten(1)
       :totable()
     api.nvim_create_autocmd("FileType", {
-      pattern = pattern,
+      pattern = filetypes,
       callback = function(args)
         vim.treesitter.start(args.buf)
-      end,
-    })
-
-    api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-      pattern = { "*.component.html", "*.container.html" },
-      callback = function(args)
-        vim.treesitter.start(args.buf, "angular")
       end,
     })
   end,
