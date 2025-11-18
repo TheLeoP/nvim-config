e0da9de53af1631a286e433949cb0b321fa0e39c
TheLeoP <eugenio2305@hotmail.com>
2025-11-17 22:51:14 -0500

    fix(overseer): follow upstream changes in future 2.0

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,10 +1,8 @@
 local function open_and_close()
-  local overseer = require "overseer"
-
-  overseer.open { enter = false }
+  require("overseer").open { enter = false }
 
   vim.defer_fn(function()
-    if vim.bo.filetype ~= "OverseerList" and vim.bo.buftype ~= "terminal" then overseer.close() end
+    if vim.bo.filetype ~= "OverseerList" and vim.bo.buftype ~= "terminal" then require("overseer").close() end
   end, 10 * 1000)
 end
 
@@ -23,19 +21,19 @@ local function start_prompt()
   local overseer = require "overseer"
   vim.ui.input({ prompt = "cmd: ", completion = "file" }, function(input)
     if not input or input == "" then return end
-    overseer.run_template {
-      name = "shell",
-      prompt = "never",
-      params = {
-        cmd = input,
-        components = {
-          { "display_duration", detail_level = 2 },
-          { "on_exit_set_status", success_codes = { 0, 1 } },
-          "on_complete_notify",
-          "on_complete_dispose",
-        },
+
+    local task = overseer.new_task {
+      cmd = input,
+      components = {
+        { "on_exit_set_status" },
+        "on_complete_notify",
+        "on_complete_dispose",
       },
+      render = function(task)
+        return require("overseer.render").format_compact(task)
+      end,
     }
+    task:start()
   end)
 end
 
@@ -43,22 +41,26 @@ return {
   {
     "stevearc/overseer.nvim",
     ---@type overseer.Config
+    ---@diagnostic disable-next-line: missing-fields
     opts = {
-      strategy = "terminal",
+      output = {
+        use_terminal = true,
+      },
       dap = true,
+      ---@diagnostic disable-next-line: missing-fields
       task_list = {
-        default_detail = 2,
         direction = "bottom",
         max_width = { 600, 0.7 },
-        bindings = {
-          ["<C-t>"] = "<CMD>OverseerQuickAction zoom<CR>",
-          ["<C-b>"] = "ScrollOutputUp",
-          ["<C-f>"] = "ScrollOutputDown",
-          ["H"] = "IncreaseAllDetail",
-          ["L"] = "DecreaseAllDetail",
-          ["g?"] = false,
-          ["<C-l>"] = false,
-          ["<C-h>"] = false,
+        keymaps = {
+          ["<C-t>"] = {
+            "keymap.run_action",
+            opts = { action = "zoom" },
+            desc = "Open task output in new tab and close task list",
+          },
+          ["<up>"] = "keymap.prev_task",
+          ["<down>"] = "keymap.next_task",
+          ["?"] = false,
+          ["q"] = false,
           ["{"] = false,
           ["}"] = false,
         },
@@ -70,20 +72,22 @@ return {
         "personal.cs.test",
         "personal.cs.run",
       },
+      ---@diagnostic disable-next-line: missing-fields
       form = {
         win_opts = { winblend = 0 },
       },
       confirm = {
         win_opts = { winblend = 5 },
       },
+      ---@diagnostic disable-next-line: missing-fields
       task_win = {
         win_opts = { winblend = 5 },
       },
       actions = {
         zoom = {
-          desc = "Open terminal in new tab and close task list",
+          desc = "Open task output in new tab and close task list",
           condition = function(task)
-            return task:get_bufnr()
+            return task:get_bufnr() ~= nil
           end,
           run = function(task)
             require("overseer").close()
@@ -101,7 +105,7 @@ return {
 
       vim.keymap.set("n", "<leader>o<", restart_last_task, { desc = "Restart last task" })
       vim.keymap.set("n", "<leader>or", function()
-        overseer.run_template({}, function(task)
+        overseer.run_task({}, function(task)
           if not task then return end
           open_and_close()
         end)
