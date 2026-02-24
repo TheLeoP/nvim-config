b94bc99ece99df6b51d62c2175c9fcbcad926fdc
TheLeoP <eugenio2305@hotmail.com>
2026-02-24 13:09:30 -0500

    feat!: remove editable-term.lua

--- plugin/editable-term.lua
+++
@@ -1,9 +0,0 @@
-require("personal.editable-term").setup {
-  term_keys = {
-    goto_line_start = "<c-x><c-a>",
-    goto_line_end = "<c-x><c-e>",
-    clear_current_line = "<c-x><c-u>",
-    forward_char = "<c-x><c-f>",
-    clear_suggestions = "<c-x><c-f>",
-  },
-}
