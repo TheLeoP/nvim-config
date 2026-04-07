709630699f5c8ec64885e648f4e2a8ec2d888acc
TheLeoP <eugenio2305@hotmail.com>
2026-04-06 21:29:57 -0500

    feat: use builtin highlighter instead of ccc.nvim's one

--- plugin/ccc.lua
+++ plugin/ccc.lua
@@ -1,20 +1,3 @@
 vim.pack.add { "https://github.com/uga-rosa/ccc.nvim" }
 
-require("ccc").setup {
-  highlighter = {
-    auto_enable = true,
-    lsp = false,
-    filetypes = {
-      "css",
-      "javascript",
-      "typescript",
-      "typescriptreact",
-      "javascriptreact",
-      "vue",
-      "html",
-      "dbout",
-      "sql",
-      "lua",
-    },
-  },
-}
+require("ccc").setup {}
