01a30d5136c8eb53bd2d1c4f77762fbd94d0f881
TheLeoP <eugenio2305@hotmail.com>
2026-05-03 10:21:31 -0500

    refactor: move fzf-lua personal pickers to plugin file

--- plugin/fzf-lua.lua
+++ plugin/fzf-lua.lua
@@ -15,17 +15,6 @@ api.nvim_del_user_command "FZF"
 
 local keymap = vim.keymap
 
----@param idx integer
-local spell_on_choice = vim.schedule_wrap(function(idx)
-  if type(idx) ~= "number" then return end
-  vim.cmd.normal { ("%dz="):format(idx), bang = true }
-end)
-
-local spellsuggest_select = function()
-  if vim.v.count > 0 then return spell_on_choice(vim.v.count) end
-  require("fzf-lua").spell_suggest()
-end
-
 local fzf = require "fzf-lua"
 fzf.setup {
   "telescope",
@@ -130,7 +119,63 @@ keymap.set("n", "<leader>fL", function()
   fzf.live_grep { silent = true, prompt = "< Rg plugins >", cwd = vim.fn.stdpath "data" .. "/site/pack/core/opt" }
 end, { desc = "Rg in plugins dir" })
 
-keymap.set("n", "<leader>fp", require("personal.fzf-lua").projects, { desc = "Find projects" })
+local function projects()
+  local recent_task = require("project_nvim").get_recent()
+  recent_task:wait(function(err, recent)
+    if err then return vim.notify(err, vim.log.levels.ERROR) end
+    local preview = vim.fn.executable "eza" == 1 and "eza -la --color=always --icons -g --group-directories-first {1}"
+      or "ls -la {1}"
+    if vim.fn.has "win32" == 1 then preview = "dir {1}" end
+    require("fzf-lua").fzf_exec(recent, {
+      actions = {
+        ["default"] = function(selected)
+          local config = require "session_manager.config"
+          local project_path = selected[1] ---@type string
+
+          vim.cmd.tcd { args = { project_path }, mods = { silent = true } }
+
+          local session_name = config.dir_to_session_filename(vim.loop.cwd()) --- @type {exists: fun():boolean}
+          if session_name:exists() then
+            require("session_manager").load_current_dir_session(true)
+          else
+            require("fzf-lua").files { cwd = project_path }
+          end
+        end,
+        ["ctrl-f"] = function(selected)
+          local project_path = selected[1] ---@type string
+          require("fzf-lua").files { cwd = project_path }
+        end,
+        ["alt-F"] = function(selected)
+          local project_path = selected[1] ---@type string
+          require("fzf-lua").live_grep { cwd = project_path }
+        end,
+        ["ctrl-d"] = {
+          fn = function(selected)
+            require("project_nvim.utils.history").delete(selected[1])
+          end,
+          reload = true,
+        },
+      },
+      preview = preview,
+      fzf_opts = {
+        ["--tiebreak"] = "index",
+      },
+    })
+  end)
+end
+
+keymap.set("n", "<leader>fp", projects, { desc = "Find projects" })
+
+---@param idx integer
+local spell_on_choice = vim.schedule_wrap(function(idx)
+  if type(idx) ~= "number" then return end
+  vim.cmd.normal { ("%dz="):format(idx), bang = true }
+end)
+
+local spellsuggest_select = function()
+  if vim.v.count > 0 then return spell_on_choice(vim.v.count) end
+  require("fzf-lua").spell_suggest()
+end
 
 keymap.set("n", "z=", spellsuggest_select, { desc = "Shows spelling suggestions" })
 
