f243fe34e349ee64209ad8b2f2b8d8f0ec586879
TheLeoP <eugenio2305@hotmail.com>
2026-07-15 08:58:28 -0500

    feat(ccc): enable only rgb and oklch as pickers

--- plugin/ccc.lua
+++ plugin/ccc.lua
@@ -1,3 +1,9 @@
 vim.pack.add { "https://github.com/uga-rosa/ccc.nvim" }
 
-require("ccc").setup {}
+local ccc = require "ccc"
+ccc.setup {
+  inputs = {
+    ccc.input.rgb,
+    ccc.input.oklch,
+  },
+}
