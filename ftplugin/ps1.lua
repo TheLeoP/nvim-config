f1de0fee2b79bda27694000bc2ee02a9683ca2d0
TheLeoP <eugenio2305@hotmail.com>
2026-08-09 22:00:13 -0500

    feat: use powershell.nvim new eval operator

--- ftplugin/ps1.lua
+++ ftplugin/ps1.lua
@@ -4,8 +4,12 @@ end, { buffer = true })
 vim.keymap.set("n", "<leader>ld", function()
   require("powershell").toggle_debug_term()
 end, { buffer = true })
-vim.keymap.set({ "n", "x" }, "<leader>le", function()
-  require("powershell").eval()
-end, { buffer = true })
+
+vim.keymap.set({ "n", "x" }, "g=", function()
+  return require("powershell").eval_operator()
+end, { buffer = true, expr = true })
+vim.keymap.set({ "n" }, "g==", function()
+  return require("powershell").eval_operator() .. "_"
+end, { buffer = true, expr = true })
 
 vim.bo.iskeyword = vim.bo.iskeyword .. ",$"
