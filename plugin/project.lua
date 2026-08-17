b260cd945bcf79d1f79d310064931910e8f33631
TheLeoP <eugenio2305@hotmail.com>
2026-08-17 18:11:15 -0500

    feat(project): add more ignored LSs

--- plugin/project.lua
+++ plugin/project.lua
@@ -3,7 +3,7 @@ vim.pack.add { "https://github.com/TheLeoP/project.nvim" }
 
 require("project_nvim").setup {
   detection_methods = { "lsp", "pattern" },
-  ignore_lsp = { "lemminx", "dockerls", "kulala" },
+  ignore_lsp = { "lemminx", "dockerls", "kulala", "yamlls", "helm_ls" },
   scope_chdir = "tab",
   patterns = {
     "!>packages",
